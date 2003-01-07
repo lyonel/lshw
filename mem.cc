8338ed8719c0d90430200d13e0d742b8257c6298
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-01-07 00:52:36 +0000

    added support for PowerMac memory banks display (via OF device-tree in /proc) to allow detailed info like: venus   +-memory - size: 640MB      +-DIMM0/J19 - size: 64MB      +-DIMM1/J20 - size: 64MB      +-DIMM2/J21 - size: 256MB      +-DIMM3/J22 - size: 256MB
    git-svn-id: http://ezix.org/source/packages/lshw/development@3 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- mem.cc
+++ mem.cc
@@ -1,6 +1,7 @@
 #include "mem.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 bool scan_memory(hwNode & n)
@@ -13,6 +14,51 @@ bool scan_memory(hwNode & n)
 		  hw::memory);
 
     memory.setSize(buf.st_size);
+
+    if (stat("/proc/device-tree/memory@0/slot-names", &buf) == 0)
+    {
+      unsigned long bitmap = 0;
+      char *slotnames = NULL;
+      char *slotname = NULL;
+      int fd = open("/proc/device-tree/memory@0/slot-names", O_RDONLY);
+      int fd2 = open("/proc/device-tree/memory@0/reg", O_RDONLY);
+
+      if ((fd >= 0) && (fd2 >= 0))
+      {
+	unsigned long slot = 1;
+	slotnames = (char *) malloc(buf.st_size + 1);
+	slotname = slotnames;
+	memset(slotnames, 0, buf.st_size + 1);
+	read(fd, &bitmap, sizeof(bitmap));
+	read(fd, slotnames, buf.st_size + 1);
+
+	while (strlen(slotname) > 0)
+	{
+	  unsigned long base = 0;
+	  unsigned long size = 0;
+	  if (bitmap & slot)	// slot is active
+	  {
+	    read(fd2, &base, sizeof(base));
+	    read(fd2, &size, sizeof(size));
+
+	    if (size > 0)
+	    {
+	      hwNode bank(slotname,
+			  hw::memory);
+
+	      bank.setSize(size);
+	      memory.addChild(bank);
+	    }
+	  }
+	  slot *= 2;
+	  slotname += strlen(slotname) + 1;
+	}
+	close(fd);
+	close(fd2);
+
+	free(slotnames);
+      }
+    }
 
     return n.addChild(memory);
   }
