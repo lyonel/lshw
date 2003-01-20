965b6c1692a307edc6c91401dee511be7253d009
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-01-20 22:54:02 +0000

    cleanup in device-tree handling
    git-svn-id: http://ezix.org/source/packages/lshw/development@80 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- device-tree.cc
+++ device-tree.cc
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #define DEVICETREE "/proc/device-tree"
@@ -14,17 +15,22 @@ static void scan_devtree_memory(hwNode & core)
 
   while (true)
   {
-    string mcbase =
-      string(DEVICETREE "/memory@") + string('0' + currentmc, 1);
+    char buffer[10];
+
+    snprintf(buffer, sizeof(buffer), "%d", currentmc);
+    string mcbase = string(DEVICETREE "/memory@") + string(buffer);
     string devtreeslotnames = mcbase + string("/slot-names");
     string reg = mcbase + string("/reg");
+
+    if (stat(devtreeslotnames.c_str(), &buf) != 0)
+      break;
 
     if (!memory || (currentmc != 0))
     {
       memory = core.addChild(hwNode("memory", hw::memory));
     }
 
-    if (memory && (stat(devtreeslotnames.c_str(), &buf) == 0))
+    if (memory)
     {
       unsigned long bitmap = 0;
       char *slotnames = NULL;
@@ -91,56 +97,7 @@ bool scan_device_tree(hwNode & n)
 
   if (core)
   {
-    hwNode *memory = core->getChild("memory");
-
-    if (!memory)
-    {
-      core->addChild(hwNode("memory", hw::memory));
-      memory = core->getChild("memory");
-    }
-
-    if (memory && (stat(DEVICETREE "/memory@0/slot-names", &buf) == 0))
-    {
-      unsigned long bitmap = 0;
-      char *slotnames = NULL;
-      char *slotname = NULL;
-      int fd = open(DEVICETREE "/memory@0/slot-names", O_RDONLY);
-      int fd2 = open(DEVICETREE "/memory@0/reg", O_RDONLY);
-
-      if ((fd >= 0) && (fd2 >= 0))
-      {
-	unsigned long slot = 1;
-	slotnames = (char *) malloc(buf.st_size + 1);
-	slotname = slotnames;
-	memset(slotnames, 0, buf.st_size + 1);
-	read(fd, &bitmap, sizeof(bitmap));
-	read(fd, slotnames, buf.st_size + 1);
-
-	while (strlen(slotname) > 0)
-	{
-	  unsigned long base = 0;
-	  unsigned long size = 0;
-	  if (bitmap & slot)	// slot is active
-	  {
-	    hwNode bank("bank",
-			hw::memory);
-
-	    read(fd2, &base, sizeof(base));
-	    read(fd2, &size, sizeof(size));
-
-	    bank.setSlot(slotname);
-	    bank.setSize(size);
-	    memory->addChild(bank);
-	  }
-	  slot *= 2;
-	  slotname += strlen(slotname) + 1;
-	}
-	close(fd);
-	close(fd2);
-
-	free(slotnames);
-      }
-    }
+    scan_devtree_memory(*core);
   }
 
   return true;
