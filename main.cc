bca113cb975e88c56df42353b8d72451ac452368
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-01-30 17:18:26 +0000

    finally added a -html option to activate HTML mode
    git-svn-id: http://ezix.org/source/packages/lshw/development@117 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- main.cc
+++ main.cc
@@ -9,11 +9,49 @@
 #include "pci.h"
 
 #include <unistd.h>
+#include <stdio.h>
+
+void usage(const char *progname)
+{
+  fprintf(stderr, "Harware Lister - lshw - %s\n", getpackageversion());
+  fprintf(stderr, "usage: %s [-options ...]\n", progname);
+  fprintf(stderr, "\t-version      print program version\n");
+  fprintf(stderr, "\t-html         output hardware tree as HTML\n");
+  fprintf(stderr, "\n");
+}
 
 int main(int argc,
 	 char **argv)
 {
   char hostname[80];
+  bool htmloutput = false;
+
+  if (argc > 2)
+  {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  if (argc == 2)
+  {
+    if (strcmp(argv[1], "-version") == 0)
+    {
+      printf("%s\n", getpackageversion());
+      exit(0);
+    }
+    if (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "--help") == 0)
+    {
+      usage(argv[0]);
+      exit(0);
+    }
+    if (strcmp(argv[1], "-html") == 0)
+      htmloutput = true;
+    else
+    {
+      usage(argv[0]);
+      exit(1);
+    }
+  }
 
   if (gethostname(hostname, sizeof(hostname)) == 0)
   {
@@ -26,10 +64,10 @@ int main(int argc,
     scan_cpuinfo(computer);
     scan_pci(computer);
 
-    print(computer, false);
+    print(computer, htmloutput);
   }
 
   return 0;
 }
 
-static char *id = "@(#) $Id: main.cc,v 1.14 2003/01/30 00:01:08 ezix Exp $";
+static char *id = "@(#) $Id: main.cc,v 1.15 2003/01/30 17:18:26 ezix Exp $";
