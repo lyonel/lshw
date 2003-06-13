0343e932e883a1d78857ddd7db974f08ee0069fc
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-06-13 22:23:45 +0000

    code cleanup
    git-svn-id: http://ezix.org/source/packages/lshw/development@256 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- network.cc
+++ network.cc
@@ -2,13 +2,9 @@
 #include "osutils.h"
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-<<<<<<< network.cc
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <net/if_arp.h>
-  == == == =
-#include <net/if_arp.h>
-  >>>>>>>1.3
 #include <linux/sockios.h>
 #include <net/if.h>
 #include <fcntl.h>
@@ -17,9 +13,8 @@
 #include <string.h>
 #include <string>
 #include <sys/types.h>
-  using namespace std;
+using namespace std;
 
-<<<<<<<network.cc
 #ifndef SIOCETHTOOL
 #define SIOCETHTOOL     0x8946
 #endif
@@ -68,56 +63,7 @@ struct ethtool_drvinfo
 #define ETHTOOL_GSET            0x00000001	/* Get settings. */
 #define ETHTOOL_GDRVINFO        0x00000003	/* Get driver info. */
 
-== == == =
-#ifndef SIOCETHTOOL
-#define SIOCETHTOOL     0x8946
-#endif
-typedef unsigned long long u64;
-typedef __uint32_t u32;
-typedef __uint16_t u16;
-typedef __uint8_t u8;
-
-struct ethtool_cmd
-{
-  u32 cmd;
-  u32 supported;		/* Features this interface supports */
-  u32 advertising;		/* Features this interface advertises */
-  u16 speed;			/* The forced speed, 10Mb, 100Mb, gigabit */
-  u8 duplex;			/* Duplex, half or full */
-  u8 port;			/* Which connector port */
-  u8 phy_address;
-  u8 transceiver;		/* Which tranceiver to use */
-  u8 autoneg;			/* Enable or disable autonegotiation */
-  u32 maxtxpkt;			/* Tx pkts before generating tx int */
-  u32 maxrxpkt;			/* Rx pkts before generating rx int */
-  u32 reserved[4];
-};
-
-#define ETHTOOL_BUSINFO_LEN     32
-/* these strings are set to whatever the driver author decides... */
-struct ethtool_drvinfo
-{
-  u32 cmd;
-  char driver[32];		/* driver short name, "tulip", "eepro100" */
-  char version[32];		/* driver version string */
-  char fw_version[32];		/* firmware version string, if applicable */
-  char bus_info[ETHTOOL_BUSINFO_LEN];	/* Bus info for this IF. */
-  /*
-   * For PCI devices, use pci_dev->slot_name. 
-   */
-  char reserved1[32];
-  char reserved2[16];
-  u32 n_stats;			/* number of u64's from ETHTOOL_GSTATS */
-  u32 testinfo_len;
-  u32 eedump_len;		/* Size of data from ETHTOOL_GEEPROM (bytes) */
-  u32 regdump_len;		/* Size of data from ETHTOOL_GREGS (bytes) */
-};
-
-/* CMDs currently supported */
-#define ETHTOOL_GSET            0x00000001	/* Get settings. */
-#define ETHTOOL_GDRVINFO        0x00000003	/* Get driver info. */
-
->>>>>>>1.3 bool load_interfaces(vector < string > &interfaces)
+bool load_interfaces(vector < string > &interfaces)
 {
   vector < string > procnetdev;
 
@@ -202,35 +148,6 @@ static string print_ip(struct sockaddr *in)
   return string(inet_ntoa(*(in_addr *) in));
 }
 
-static const char *hwname(int t)
-{
-  switch (t)
-  {
-  case ARPHRD_ETHER:
-    return "ethernet";
-  case ARPHRD_SLIP:
-    return "slip";
-  case ARPHRD_LOOPBACK:
-    return "loopback";
-  case ARPHRD_FDDI:
-    return "fddi";
-  case ARPHRD_IRDA:
-    return "irda";
-  case ARPHRD_PPP:
-    return "ppp";
-  case ARPHRD_X25:
-    return "x25";
-  case ARPHRD_TUNNEL:
-    return "iptunnel";
-  case ARPHRD_DLCI:
-    return "framerelay.dlci";
-  case ARPHRD_FRAD:
-    return "framerelay.ad";
-  default:
-    return "";
-  }
-}
-
 bool scan_network(hwNode & n)
 {
   vector < string > interfaces;
@@ -262,7 +179,8 @@ bool scan_network(hwNode & n)
 	// IP address is in ifr.ifr_addr
 	interface.setConfig("ip", print_ip(&ifr.ifr_addr));
 	strcpy(ifr.ifr_name, interfaces[i].c_str());
-	if (ioctl(fd, SIOCGIFDSTADDR, &ifr) == 0)
+	if ((interface.getConfig("point-to-point") == "yes")
+	    && (ioctl(fd, SIOCGIFDSTADDR, &ifr) == 0))
 	{
 	  // remote PPP address is in ifr.ifr_dstaddr
 	  interface.setConfig("remoteip", print_ip(&ifr.ifr_dstaddr));
@@ -344,4 +262,4 @@ bool scan_network(hwNode & n)
     return false;
 }
 
-static char *id = "@(#) $Id: network.cc,v 1.4 2003/06/13 22:18:02 ezix Exp $";
+static char *id = "@(#) $Id: network.cc,v 1.5 2003/06/13 22:23:45 ezix Exp $";
