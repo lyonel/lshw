ca2760e8d01513d0a48f076aae754c49ac7213ad
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-06-13 22:18:02 +0000

    fixed conflicts
    git-svn-id: http://ezix.org/source/packages/lshw/development@255 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- network.cc
+++ network.cc
@@ -2,7 +2,13 @@
 #include "osutils.h"
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+<<<<<<< network.cc
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <net/if_arp.h>
+  == == == =
+#include <net/if_arp.h>
+  >>>>>>>1.3
 #include <linux/sockios.h>
 #include <net/if.h>
 #include <fcntl.h>
@@ -11,13 +17,12 @@
 #include <string.h>
 #include <string>
 #include <sys/types.h>
+  using namespace std;
 
-using namespace std;
-
+<<<<<<<network.cc
 #ifndef SIOCETHTOOL
 #define SIOCETHTOOL     0x8946
 #endif
-
 typedef unsigned long long u64;
 typedef __uint32_t u32;
 typedef __uint16_t u16;
@@ -63,7 +68,56 @@ struct ethtool_drvinfo
 #define ETHTOOL_GSET            0x00000001	/* Get settings. */
 #define ETHTOOL_GDRVINFO        0x00000003	/* Get driver info. */
 
-bool load_interfaces(vector < string > &interfaces)
+== == == =
+#ifndef SIOCETHTOOL
+#define SIOCETHTOOL     0x8946
+#endif
+typedef unsigned long long u64;
+typedef __uint32_t u32;
+typedef __uint16_t u16;
+typedef __uint8_t u8;
+
+struct ethtool_cmd
+{
+  u32 cmd;
+  u32 supported;		/* Features this interface supports */
+  u32 advertising;		/* Features this interface advertises */
+  u16 speed;			/* The forced speed, 10Mb, 100Mb, gigabit */
+  u8 duplex;			/* Duplex, half or full */
+  u8 port;			/* Which connector port */
+  u8 phy_address;
+  u8 transceiver;		/* Which tranceiver to use */
+  u8 autoneg;			/* Enable or disable autonegotiation */
+  u32 maxtxpkt;			/* Tx pkts before generating tx int */
+  u32 maxrxpkt;			/* Rx pkts before generating rx int */
+  u32 reserved[4];
+};
+
+#define ETHTOOL_BUSINFO_LEN     32
+/* these strings are set to whatever the driver author decides... */
+struct ethtool_drvinfo
+{
+  u32 cmd;
+  char driver[32];		/* driver short name, "tulip", "eepro100" */
+  char version[32];		/* driver version string */
+  char fw_version[32];		/* firmware version string, if applicable */
+  char bus_info[ETHTOOL_BUSINFO_LEN];	/* Bus info for this IF. */
+  /*
+   * For PCI devices, use pci_dev->slot_name. 
+   */
+  char reserved1[32];
+  char reserved2[16];
+  u32 n_stats;			/* number of u64's from ETHTOOL_GSTATS */
+  u32 testinfo_len;
+  u32 eedump_len;		/* Size of data from ETHTOOL_GEEPROM (bytes) */
+  u32 regdump_len;		/* Size of data from ETHTOOL_GREGS (bytes) */
+};
+
+/* CMDs currently supported */
+#define ETHTOOL_GSET            0x00000001	/* Get settings. */
+#define ETHTOOL_GDRVINFO        0x00000003	/* Get driver info. */
+
+>>>>>>>1.3 bool load_interfaces(vector < string > &interfaces)
 {
   vector < string > procnetdev;
 
@@ -94,10 +148,13 @@ static string getmac(const unsigned char *mac)
 {
   char buff[5];
   string result = "";
+  bool valid = false;
 
   for (int i = 0; i < 6; i++)
   {
     snprintf(buff, sizeof(buff), "%02x", mac[i]);
+
+    valid |= (mac[i] != 0);
 
     if (i == 0)
       result = string(buff);
@@ -105,7 +162,44 @@ static string getmac(const unsigned char *mac)
       result += ":" + string(buff);
   }
 
-  return result;
+  if (valid)
+    return result;
+  else
+    return "";
+}
+
+static const char *hwname(int t)
+{
+  switch (t)
+  {
+  case ARPHRD_ETHER:
+    return "ethernet";
+  case ARPHRD_SLIP:
+    return "slip";
+  case ARPHRD_LOOPBACK:
+    return "loopback";
+  case ARPHRD_FDDI:
+    return "fddi";
+  case ARPHRD_IRDA:
+    return "irda";
+  case ARPHRD_PPP:
+    return "ppp";
+  case ARPHRD_X25:
+    return "x25";
+  case ARPHRD_TUNNEL:
+    return "iptunnel";
+  case ARPHRD_DLCI:
+    return "framerelay.dlci";
+  case ARPHRD_FRAD:
+    return "framerelay.ad";
+  default:
+    return "";
+  }
+}
+
+static string print_ip(struct sockaddr *in)
+{
+  return string(inet_ntoa(*(in_addr *) in));
 }
 
 static const char *hwname(int t)
@@ -159,9 +253,58 @@ bool scan_network(hwNode & n)
       interface.setLogicalName(interfaces[i]);
       interface.claim();
 
+      // get IP address
       memset(&ifr, 0, sizeof(ifr));
       strcpy(ifr.ifr_name, interfaces[i].c_str());
+      ifr.ifr_addr.sa_family = AF_INET;
+      if (ioctl(fd, SIOCGIFADDR, &ifr) == 0)
+      {
+	// IP address is in ifr.ifr_addr
+	interface.setConfig("ip", print_ip(&ifr.ifr_addr));
+	strcpy(ifr.ifr_name, interfaces[i].c_str());
+	if (ioctl(fd, SIOCGIFDSTADDR, &ifr) == 0)
+	{
+	  // remote PPP address is in ifr.ifr_dstaddr
+	  interface.setConfig("remoteip", print_ip(&ifr.ifr_dstaddr));
+	}
+      }
 
+      memset(&ifr, 0, sizeof(ifr));
+      strcpy(ifr.ifr_name, interfaces[i].c_str());
+      if (ioctl(fd, SIOCGIFFLAGS, &ifr) == 0)
+      {
+#ifdef IFF_PORTSEL
+	if (ifr.ifr_flags & IFF_PORTSEL)
+	{
+	  if (ifr.ifr_flags & IFF_AUTOMEDIA)
+	    interface.setConfig("automedia", "yes");
+	}
+#endif
+
+	if (ifr.ifr_flags & IFF_UP)
+	  interface.enable();
+	else
+	  interface.disable();
+	if (ifr.ifr_flags & IFF_BROADCAST)
+	  interface.setConfig("broadcast", "yes");
+	if (ifr.ifr_flags & IFF_DEBUG)
+	  interface.setConfig("debug", "yes");
+	if (ifr.ifr_flags & IFF_LOOPBACK)
+	  interface.setConfig("loopback", "yes");
+	if (ifr.ifr_flags & IFF_POINTOPOINT)
+	  interface.setConfig("point-to-point", "yes");
+	if (ifr.ifr_flags & IFF_PROMISC)
+	  interface.setConfig("promiscuous", "yes");
+	if (ifr.ifr_flags & IFF_SLAVE)
+	  interface.setConfig("slave", "yes");
+	if (ifr.ifr_flags & IFF_MASTER)
+	  interface.setConfig("master", "yes");
+	if (ifr.ifr_flags & IFF_MULTICAST)
+	  interface.setConfig("multicast", "yes");
+      }
+
+      memset(&ifr, 0, sizeof(ifr));
+      strcpy(ifr.ifr_name, interfaces[i].c_str());
       // get MAC address
       if (ioctl(fd, SIOCGIFHWADDR, &ifr) == 0)
       {
@@ -171,6 +314,8 @@ bool scan_network(hwNode & n)
       }
 
       drvinfo.cmd = ETHTOOL_GDRVINFO;
+      memset(&ifr, 0, sizeof(ifr));
+      strcpy(ifr.ifr_name, interfaces[i].c_str());
       ifr.ifr_data = (caddr_t) & drvinfo;
       if (ioctl(fd, SIOCETHTOOL, &ifr) == 0)
       {
@@ -185,7 +330,11 @@ bool scan_network(hwNode & n)
 	existing->merge(interface);
       }
       else
-	n.addChild(interface);
+      {
+	// we don't care about loopback interfaces
+	if (!interface.isCapable("loopback"))
+	  n.addChild(interface);
+      }
     }
 
     close(fd);
@@ -195,4 +344,4 @@ bool scan_network(hwNode & n)
     return false;
 }
 
-static char *id = "@(#) $Id: network.cc,v 1.3 2003/06/13 07:15:15 ezix Exp $";
+static char *id = "@(#) $Id: network.cc,v 1.4 2003/06/13 22:18:02 ezix Exp $";
