e5092deff2fcc0021169940abd5077449f671a96
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-11-17 22:29:57 +0000

    more work on USB
    git-svn-id: http://ezix.org/source/packages/lshw/development@321 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- usb.cc
+++ usb.cc
@@ -7,6 +7,8 @@
 #include "osutils.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
@@ -14,6 +16,8 @@
 #define PROCBUSUSB "/proc/bus/usb/"
 
 #define	USB_DT_DEVICE_SIZE	0x12
+#define USB_CTRL_TIMEOUT    100	/* milliseconds */
+#define USB_CTRL_RETRIES     50
 
 #define USB_CLASS_PER_INTERFACE         0	/* for DeviceClass */
 #define USB_CLASS_AUDIO                 1
@@ -53,7 +57,52 @@ struct usb_device_descriptor
 }
 __attribute__ ((packed));
 
-static char *id = "@(#) $Id: usb.cc,v 1.1 2003/11/12 12:42:59 ezix Exp $";
+#define USBDEVICE_SUPER_MAGIC 0x9fa2
+
+/* usbdevfs ioctl codes */
+
+struct usbdevfs_ctrltransfer
+{
+  u_int8_t requesttype;
+  u_int8_t request;
+  u_int16_t value;
+  u_int16_t index;
+  u_int16_t length;
+  u_int32_t timeout;		/* in milliseconds */
+  void *data;
+};
+
+#define USBDEVFS_CONTROL	_IOWR('U', 0, struct usbdevfs_ctrltransfer)
+
+static char *id = "@(#) $Id: usb.cc,v 1.2 2003/11/17 22:29:57 ezix Exp $";
+
+static int usb_control_msg(int fd,
+			   u_int8_t requesttype,
+			   u_int8_t request,
+			   u_int16_t value,
+			   u_int16_t index,
+			   unsigned int size,
+			   void *data)
+{
+  struct usbdevfs_ctrltransfer ctrl;
+  int result, tries;
+
+  ctrl.requesttype = requesttype;
+  ctrl.request = request;
+  ctrl.value = value;
+  ctrl.index = index;
+  ctrl.length = size;
+  ctrl.data = data;
+  ctrl.timeout = USB_CTRL_TIMEOUT;
+  tries = 0;
+  do
+  {
+    result = ioctl(fd, USBDEVFS_CONTROL, &ctrl);
+    tries++;
+  }
+  while (tries < USB_CTRL_RETRIES && result == -1 && errno == ETIMEDOUT);
+  return result;
+}
 
 static int selectnumber(const struct dirent *d)
 {
@@ -105,11 +154,12 @@ bool scan_usb(hwNode & n)
 
       for (int j = 0; j < countdevices; j++)
       {
-	hwNode device("device");
+	hwNode device("usb");
 	usb_device_descriptor descriptor;
 	int fd = -1;
 
-	device.setPhysId(devices[j]->d_name);
+	// device.setPhysId(devices[j]->d_name);
+	device.setHandle(string("USB:") + string(devices[j]->d_name));
 
 	memset(&descriptor, sizeof(descriptor), 0);
 	fd = open(devices[j]->d_name, O_RDONLY);
@@ -119,6 +169,23 @@ bool scan_usb(hwNode & n)
 	  {
 	    if (descriptor.bLength == USB_DT_DEVICE_SIZE)
 	    {
+	      char usbversion[20];
+
+	      snprintf(usbversion, sizeof(usbversion), "usb-%x.%02x",
+		       (descriptor.bcdUSB & 0xff00) >> 8,
+		       descriptor.bcdUSB & 0x00ff);
+	      device.addCapability(usbversion);
+
+	      if ((descriptor.bcdUSB & 0x00ff) == 0)	// like USB 1.0
+	      {
+		snprintf(usbversion, sizeof(usbversion), "usb-%x.0",
+			 (descriptor.bcdUSB & 0xff00) >> 8);
+		device.addCapability(usbversion);	// usb-1
+		snprintf(usbversion, sizeof(usbversion), "usb-%x",
+			 (descriptor.bcdUSB & 0xff00) >> 8);
+		device.addCapability(usbversion);	// usb-1.0
+	      }
+
 	      switch (descriptor.bDeviceClass)
 	      {
 	      case USB_CLASS_AUDIO:
@@ -146,6 +213,9 @@ bool scan_usb(hwNode & n)
 		device.setDescription("USB hub");
 		break;
 	      }
+
+	      if (descriptor.idVendor == 0)
+		device.addCapability("virtual");
 	    }
 	  }
 	  close(fd);
