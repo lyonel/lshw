87840940290f367b3e4c3a54224d9c765ef634be
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2003-11-18 00:02:54 +0000

    continuing working on USB
    git-svn-id: http://ezix.org/source/packages/lshw/development@322 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- usb.cc
+++ usb.cc
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <iconv.h>
 
 #define PROCBUSUSB "/proc/bus/usb/"
 
@@ -28,6 +29,38 @@
 #define USB_CLASS_HUB                   9
 #define USB_CLASS_DATA                  10
 #define USB_CLASS_VENDOR_SPEC           0xff
+
+#define USB_DIR_OUT                     0
+#define USB_DIR_IN                      0x80
+
+/*
+ * Descriptor types
+ */
+#define USB_DT_DEVICE                   0x01
+#define USB_DT_CONFIG                   0x02
+#define USB_DT_STRING                   0x03
+#define USB_DT_INTERFACE                0x04
+#define USB_DT_ENDPOINT                 0x05
+
+#define USB_DT_HID                      (USB_TYPE_CLASS | 0x01)
+#define USB_DT_REPORT                   (USB_TYPE_CLASS | 0x02)
+#define USB_DT_PHYSICAL                 (USB_TYPE_CLASS | 0x03)
+#define USB_DT_HUB                      (USB_TYPE_CLASS | 0x09)
+
+/*
+ * Standard requests
+ */
+#define USB_REQ_GET_STATUS              0x00
+#define USB_REQ_CLEAR_FEATURE           0x01
+#define USB_REQ_SET_FEATURE             0x03
+#define USB_REQ_SET_ADDRESS             0x05
+#define USB_REQ_GET_DESCRIPTOR          0x06
+#define USB_REQ_SET_DESCRIPTOR          0x07
+#define USB_REQ_GET_CONFIGURATION       0x08
+#define USB_REQ_SET_CONFIGURATION       0x09
+#define USB_REQ_GET_INTERFACE           0x0A
+#define USB_REQ_SET_INTERFACE           0x0B
+#define USB_REQ_SYNCH_FRAME             0x0C
 
 /* All standard descriptors have these 2 fields in common */
 struct usb_descriptor_header
@@ -74,7 +107,7 @@ struct usbdevfs_ctrltransfer
 
 #define USBDEVFS_CONTROL	_IOWR('U', 0, struct usbdevfs_ctrltransfer)
 
-static char *id = "@(#) $Id: usb.cc,v 1.2 2003/11/17 22:29:57 ezix Exp $";
+static char *id = "@(#) $Id: usb.cc,v 1.3 2003/11/18 00:02:54 ezix Exp $";
 
 static int usb_control_msg(int fd,
 			   u_int8_t requesttype,
@@ -102,6 +135,60 @@ static int usb_control_msg(int fd,
   }
   while (tries < USB_CTRL_RETRIES && result == -1 && errno == ETIMEDOUT);
   return result;
+}
+
+static string get_string(int fd,
+			 u_int8_t id,
+			 u_int16_t lang)
+{
+  unsigned char b[256];
+  wchar_t w[128];
+  char buf[256];
+  unsigned int i;
+  int ret;
+  iconv_t cd;
+
+  /*
+   * string ID 0 means no string 
+   */
+  if (!id || fd < 0)
+    return "";
+
+  memset(b, 0, sizeof(b));
+  b[0] = b[1] = 0xbf;
+  ret =
+    usb_control_msg(fd, USB_DIR_IN, USB_REQ_GET_DESCRIPTOR,
+		    (USB_DT_STRING << 8) | id, 0, sizeof(b), b);
+  if (ret < 0)
+  {
+    fprintf(stderr, "cannot get string descriptor %d, error = %s(%d)\n", id,
+	    strerror(errno), errno);
+    return "";
+  }
+  if (ret < 2 || b[0] < 2 || b[1] != USB_DT_STRING)
+  {
+    fprintf(stderr, "string descriptor %d invalid (%02x %02x; len=%d)\n", id,
+	    b[0], b[1], ret);
+    return "";
+  }
+
+  for (i = 0; i < ((b[0] - 2) / 2); i++)
+    w[i] = b[2 + 2 * i] | (b[3 + 2 * i] << 8);
+  w[i] = 0;
+
+  wcstombs(buf, w, sizeof(buf));
+
+  return string(buf);
+}
+
+static string BCDversion(u_int16_t v)
+{
+  char version[20];
+
+  snprintf(version, sizeof(version), "%x.%02x", (v & 0xff00) >> 8,
+	   v & 0x00ff);
+
+  return version;
 }
 
 static int selectnumber(const struct dirent *d)
@@ -159,32 +246,24 @@ bool scan_usb(hwNode & n)
 	int fd = -1;
 
 	// device.setPhysId(devices[j]->d_name);
-	device.setHandle(string("USB:") + string(devices[j]->d_name));
+	device.setHandle(string("USB:") + string(hubs[i]->d_name) + ":" +
+			 string(devices[j]->d_name));
 
 	memset(&descriptor, sizeof(descriptor), 0);
-	fd = open(devices[j]->d_name, O_RDONLY);
+	fd = open(devices[j]->d_name, O_RDWR);
 	if (fd >= 0)
 	{
 	  if (read(fd, &descriptor, USB_DT_DEVICE_SIZE) == USB_DT_DEVICE_SIZE)
 	  {
 	    if (descriptor.bLength == USB_DT_DEVICE_SIZE)
 	    {
-	      char usbversion[20];
+	      device.addCapability("usb-" + BCDversion(descriptor.bcdUSB));
+	      if (descriptor.bcdDevice)
+		device.setVersion(BCDversion(descriptor.bcdDevice));
 
-	      snprintf(usbversion, sizeof(usbversion), "usb-%x.%02x",
-		       (descriptor.bcdUSB & 0xff00) >> 8,
-		       descriptor.bcdUSB & 0x00ff);
-	      device.addCapability(usbversion);
-
-	      if ((descriptor.bcdUSB & 0x00ff) == 0)	// like USB 1.0
-	      {
-		snprintf(usbversion, sizeof(usbversion), "usb-%x.0",
-			 (descriptor.bcdUSB & 0xff00) >> 8);
-		device.addCapability(usbversion);	// usb-1
-		snprintf(usbversion, sizeof(usbversion), "usb-%x",
-			 (descriptor.bcdUSB & 0xff00) >> 8);
-		device.addCapability(usbversion);	// usb-1.0
-	      }
+	      device.setProduct(get_string(fd, descriptor.iProduct, 0));
+	      device.setVendor(get_string(fd, descriptor.iManufacturer, 0));
+	      device.setSerial(get_string(fd, descriptor.iSerialNumber, 0));
 
 	      switch (descriptor.bDeviceClass)
 	      {
