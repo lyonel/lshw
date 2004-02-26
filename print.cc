4ea128465dd80ccf38cd446798bc5a2723aaac40
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2004-02-26 16:29:32 +0000

    reverted previous (cosmetic) change
    git-svn-id: http://ezix.org/source/packages/lshw/development@344 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- print.cc
+++ print.cc
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static char *id = "@(#) $Id: print.cc,v 1.56 2004/02/26 16:26:36 ezix Exp $";
+static char *id = "@(#) $Id: print.cc,v 1.57 2004/02/26 16:29:32 ezix Exp $";
 
 static void spaces(unsigned int count,
 		   string space = " ")
@@ -746,8 +746,7 @@ static void printhwnode(hwNode & node,
   entry.path = "";
   if (node.getPhysId() != "")
     entry.path = prefix + "/" + node.getPhysId();
-  if (node.getProduct() != "")
-    entry.description = hw::strip(node.getVendor() + " " + node.getProduct());
+  entry.description = node.getProduct();
   if (entry.description == "")
     entry.description = node.getDescription();
   entry.devname = node.getLogicalName();
