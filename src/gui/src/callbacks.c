3f5e1c2e5e257a84e77c60a034675cc59f058498
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2004-11-23 23:19:42 +0000

    added Glade-generated (glade2 -w gtk-lshw.glade) files
    git-svn-id: http://ezix.org/source/packages/lshw/development@755 811e2811-9fd9-0310-a116-b6e8ac943c8b

---
+++ src/gui/src/callbacks.c
@@ -0,0 +1,50 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+save_as                                (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_copy1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+refresh_display                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_about1_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_aboutclose_activate                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
