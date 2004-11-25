55cacf972af7b558413d10e35ebd12d95986f277
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2004-11-25 23:03:50 +0000

    more code reorganisation to prepare for GUI
    git-svn-id: http://ezix.org/source/packages/lshw/development@761 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/heuristics.cc
+++ src/core/heuristics.cc
@@ -7,7 +7,7 @@
 #include "sysfs.h"
 #include "osutils.h"
 
-static char *id = "@(#) $Id: sysfs.cc 723 2004-09-23 15:51:46Z ezix $";
+static char *id = "@(#) $Id$";
 
 hwNode * guessParent(const hwNode & child, hwNode & base)
 {
