71680489f19868755be8bd633cc3c0d846719eb8
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2004-11-29 13:38:36 +0000

    git-svn-id: http://ezix.org/source/packages/lshw/development@770 811e2811-9fd9-0310-a116-b6e8ac943c8b

---
+++ src/gui/callbacks.c
@@ -0,0 +1,72 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+static GtkWidget *about = NULL;
+static GtkWidget *saveas = NULL;
+
+void
+save_as                                (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+  /*if(!GTK_IS_WIDGET(saveas))
+  {
+    saveas = create_saveas();
+    gtk_widget_show(saveas);
+  }*/
+}
+
+
+void
+on_copy1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+refresh_display                        (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_about1_activate                     (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+  if(!GTK_IS_WIDGET(about))
+  {
+    about = create_aboutlshw();
+    gtk_widget_show(about);
+  }
+}
+
+
+void
+on_aboutclose_activate          (GtkButton       *button,
+                                        gpointer         user_data)
+{
+  if(GTK_IS_WIDGET(about))
+  {
+    gtk_widget_destroy(about);
+  }
+}
+
+
+void
+on_version_realize                     (GtkWidget       *widget,
+                                        gpointer         user_data)
+{
+  gtk_label_set_text(GTK_LABEL(widget), "coucou");
+}
+
+
