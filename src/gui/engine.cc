7ae5d713f00552d412be62b0f8772ae0c1a85875
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2004-12-16 15:11:07 +0000

    GUI code cleanup
    git-svn-id: http://ezix.org/source/packages/lshw/development@830 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/gui/engine.cc
+++ src/gui/engine.cc
@@ -35,66 +35,6 @@ static void clear_list(GtkWidget * list1)
     gtk_tree_view_remove_column(GTK_TREE_VIEW(list1), col);
 }
 
-#if 0
-static void populate_list(GtkWidget * list1, hwNode * root)
-{
-  GtkListStore *list_store = NULL;
-  GtkTreeViewColumn   *col = NULL;
-  GtkCellRenderer     *renderer = NULL;
-  GtkTreeIter iter;
-  string text;
-
-  clear_list(list1);
-
-  if(!root) return;
-
-  list_store = gtk_list_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_POINTER, G_TYPE_INT, G_TYPE_STRING);
-  gtk_list_store_append(list_store, &iter);
-
-  text = root->getDescription();
-  if(text == "")
-    text = root->getProduct();
-  if(text == "")
-    text = root->getId();
-
-  gtk_list_store_set (list_store, &iter,
-                    COL_NAME, text.c_str(),
-                    COL_NODE, root,
-                    COL_WEIGHT, (root->countChildren()>0)?PANGO_WEIGHT_BOLD:PANGO_WEIGHT_NORMAL,
-                    COL_CONTINUATION, (root->countChildren()>0)?">":"",
-                    -1);
-
-  col = gtk_tree_view_column_new();
-  gtk_tree_view_append_column(GTK_TREE_VIEW(list1), col);
-  renderer = gtk_cell_renderer_text_new();
-  gtk_tree_view_column_pack_start(col, renderer, TRUE);
-  gtk_tree_view_column_add_attribute(col, renderer, "text", COL_NAME);
-  gtk_tree_view_column_add_attribute(col, renderer, "weight", COL_WEIGHT);
-
-  col = gtk_tree_view_column_new();
-  gtk_tree_view_append_column(GTK_TREE_VIEW(list1), col);
-  renderer = gtk_cell_renderer_text_new();
-  g_object_set(renderer, "xalign", (gfloat)1, NULL);
-  gtk_tree_view_column_pack_start(col, renderer, TRUE);
-  gtk_tree_view_column_add_attribute(col, renderer, "text", COL_CONTINUATION);
-  gtk_tree_view_column_add_attribute(col, renderer, "weight", COL_WEIGHT);
-
-
-#if 0
-    col = gtk_tree_view_column_new();
-    gtk_tree_view_append_column(GTK_TREE_VIEW(list1), col);
-    renderer = gtk_cell_renderer_text_new();
-    gtk_tree_view_column_pack_start(col, renderer, TRUE);
-    g_object_set(renderer, "text", ">", NULL);
-#endif
-  gtk_tree_view_set_model(GTK_TREE_VIEW(list1), GTK_TREE_MODEL(list_store));
-  g_object_unref(list_store);
-
-  gtk_tree_selection_set_mode(GTK_TREE_SELECTION(gtk_tree_view_get_selection(GTK_TREE_VIEW(list1))), GTK_SELECTION_BROWSE);
-  gtk_tree_selection_select_iter(GTK_TREE_SELECTION(gtk_tree_view_get_selection(GTK_TREE_VIEW(list1))), &iter);
-}
-#endif
-
 static void populate_sublist(GtkWidget * list1, hwNode * root, hwNode *current=NULL)
 {
   GtkListStore *list_store = NULL;
@@ -166,8 +106,6 @@ static void display(GtkWidget * mainwindow)
     gtk_label_set_text(GTK_LABEL(description), printmarkup(*displayed).c_str());
   gtk_label_set_use_markup (GTK_LABEL(description), TRUE);
   gtk_label_set_line_wrap (GTK_LABEL(description), TRUE);
-
-  //YIELD();
 }
 
 void status(const char *message)
@@ -190,11 +128,24 @@ void status(const char *message)
 void refresh(GtkWidget *mainwindow)
 {
   hwNode computer("computer", hw::system);
+  static bool lock = false;
+  GtkWidget * description = lookup_widget(mainwindow, "description");
+
+  if(lock) return;
+
+  lock = true;
 
   GtkWidget * list1 = lookup_widget(mainwindow, "treeview1");
   GtkWidget * list2 = lookup_widget(mainwindow, "treeview2");
   GtkWidget * list3 = lookup_widget(mainwindow, "treeview3");
   statusbar = lookup_widget(mainwindow, "statusbar");
+
+  populate_sublist(list1, NULL);
+  populate_sublist(list2, NULL);
+  populate_sublist(list3, NULL);
+  displayed = NULL;
+  gtk_label_set_text(GTK_LABEL(description), "<i>scanning...</i>");
+  gtk_label_set_use_markup (GTK_LABEL(description), TRUE);
 
   container = hwNode("container", hw::generic);
   status("Scanning...");
@@ -202,8 +153,6 @@ void refresh(GtkWidget *mainwindow)
   status(NULL);
   container.addChild(computer);
 
-
-  //displayed = computer.getChild("core");
 
   selected1 = NULL;
   selected2 = NULL;
@@ -213,6 +162,8 @@ void refresh(GtkWidget *mainwindow)
   populate_sublist(list2, NULL);
   populate_sublist(list3, NULL);
   display(mainwindow);
+
+  lock = false;
 }
 
 void change_selection(unsigned list, GtkTreeView *treeview)
@@ -294,13 +245,11 @@ void browse(unsigned list, GtkTreeView *treeview)
       }
       break;
     case 2:
-      //if(n == selected2) break;	// nothing to change
       selected2 = n;
       selected3 = NULL;
       populate_sublist(list3, selected2, selected3);
       break;
     case 3:
-      //if(n == selected3) break;	// nothing to change
       selected3 = n;
       if(n->countChildren()>0)
       {
@@ -314,10 +263,4 @@ void browse(unsigned list, GtkTreeView *treeview)
       }
       break;
   }
-
-  /*if(n)
-  {
-    displayed = n;
-    display(mainwindow);
-  }*/
 }
