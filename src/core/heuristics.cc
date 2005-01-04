1f0379905d6e10e51c2b0d2c924fda34817a2a49
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2005-01-04 23:36:31 +0000

    added guessing of USB-related businfo (should fix SCSI emulation for USB mass storage devices)
    git-svn-id: http://ezix.org/source/packages/lshw/development@851 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/heuristics.cc
+++ src/core/heuristics.cc
@@ -11,13 +11,26 @@ static char *id = "@(#) $Id$";
 
 string guessBusInfo(const string & info)
 {
-  if((info.length()==12) && (info[4] == ':') && (info[7] == ':') && (info[10] == '.')) // 2.6-style PCI
+  if(matches(info,"^....:..:..\\..$")) // 2.6-style PCI
   {
     return "pci@" + info.substr(info.length() - 7);
   }
-  if((info.length()==7) && (info[2] == ':') && (info[5] == '.')) // 2.4-style PCI
+  if(matches(info,"^..:..\\..$")) // 2.4-style PCI
   {
     return "pci@" + info;
+  }
+
+  if(matches(info, "^[0-9]+-[0-9]+:[0-9]+\\.[0-9]+$")) // USB: host-port:config.interface
+  {
+    unsigned host=0, port=0, config=0, interface=0;
+    char buffer[20];
+
+    sscanf(info.c_str(), "%u-%u:%u.%u", &host, &port, &config, &interface);
+    if((host==0) || (port==0)) return "";
+
+    port--;	// USB ports are numbered from 1, we want it from 0
+    snprintf(buffer, sizeof(buffer), "usb@%u:%u", host, port);
+    return string(buffer);
   }
   return "";
 }
