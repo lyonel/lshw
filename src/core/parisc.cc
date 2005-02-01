da3fd9b076af733771e4a72f6a61140d6c2bffe7
ezix <ezix@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2005-02-01 16:55:29 +0000

    accurately report PA-RISC CPUs
    git-svn-id: http://ezix.org/source/packages/lshw/development@939 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/parisc.cc
+++ src/core/parisc.cc
@@ -108,6 +108,120 @@ static struct parisc_device parisc_device_list[] = {
 	{0x00000, "generic", hw::generic, NULL},
 };
 
+enum cpu_type {
+        pcx     = 0, /* pa7000          pa 1.0  */
+        pcxs    = 1, /* pa7000          pa 1.1a */
+        pcxt    = 2, /* pa7100          pa 1.1b */
+        pcxt_   = 3, /* pa7200  (t')    pa 1.1c */
+        pcxl    = 4, /* pa7100lc        pa 1.1d */
+        pcxl2   = 5, /* pa7300lc        pa 1.1e */
+        pcxu    = 6, /* pa8000          pa 2.0  */
+        pcxu_   = 7, /* pa8200  (u+)    pa 2.0  */
+        pcxw    = 8, /* pa8500          pa 2.0  */
+        pcxw_   = 9, /* pa8600  (w+)    pa 2.0  */
+        pcxw2   = 10, /* pa8700         pa 2.0  */
+        mako    = 11  /* pa8800         pa 2.0  */
+};
+
+static struct hp_cpu_type_mask {
+        unsigned short model;
+        unsigned short mask;
+        enum cpu_type cpu;
+} hp_cpu_type_mask_list[] = {
+                                                                                
+        { 0x0000, 0x0ff0, pcx    },  /* 0x0000 - 0x000f */
+        { 0x0048, 0x0ff0, pcxl   },  /* 0x0040 - 0x004f */
+        { 0x0080, 0x0ff0, pcx    },  /* 0x0080 - 0x008f */
+        { 0x0100, 0x0ff0, pcx    },  /* 0x0100 - 0x010f */
+        { 0x0182, 0x0ffe, pcx    },  /* 0x0182 - 0x0183 */
+        { 0x0182, 0x0ffe, pcxt   },  /* 0x0182 - 0x0183 */
+        { 0x0184, 0x0fff, pcxu   },  /* 0x0184 - 0x0184 */
+        { 0x0200, 0x0ffe, pcxs   },  /* 0x0200 - 0x0201 */
+        { 0x0202, 0x0fff, pcxs   },  /* 0x0202 - 0x0202 */
+        { 0x0203, 0x0fff, pcxt   },  /* 0x0203 - 0x0203 */
+        { 0x0204, 0x0ffc, pcxt   },  /* 0x0204 - 0x0207 */
+        { 0x0280, 0x0ffc, pcxs   },  /* 0x0280 - 0x0283 */
+        { 0x0284, 0x0ffc, pcxt   },  /* 0x0284 - 0x0287 */
+        { 0x0288, 0x0fff, pcxt   },  /* 0x0288 - 0x0288 */
+        { 0x0300, 0x0ffc, pcxs   },  /* 0x0300 - 0x0303 */
+        { 0x0310, 0x0ff0, pcxt   },  /* 0x0310 - 0x031f */
+        { 0x0320, 0x0ff0, pcxt   },  /* 0x0320 - 0x032f */
+        { 0x0400, 0x0ff0, pcxt   },  /* 0x0400 - 0x040f */
+        { 0x0480, 0x0ff0, pcxl   },  /* 0x0480 - 0x048f */
+        { 0x0500, 0x0ff0, pcxl2  },  /* 0x0500 - 0x050f */
+        { 0x0510, 0x0ff0, pcxl2  },  /* 0x0510 - 0x051f */
+        { 0x0580, 0x0ff8, pcxt_  },  /* 0x0580 - 0x0587 */
+        { 0x0588, 0x0ffc, pcxt_  },  /* 0x0588 - 0x058b */
+        { 0x058c, 0x0ffe, pcxt_  },  /* 0x058c - 0x058d */
+        { 0x058e, 0x0fff, pcxt_  },  /* 0x058e - 0x058e */
+        { 0x058f, 0x0fff, pcxu   },  /* 0x058f - 0x058f */
+        { 0x0590, 0x0ffe, pcxu   },  /* 0x0590 - 0x0591 */
+        { 0x0592, 0x0fff, pcxt_  },  /* 0x0592 - 0x0592 */
+        { 0x0593, 0x0fff, pcxu   },  /* 0x0593 - 0x0593 */
+        { 0x0594, 0x0ffc, pcxu   },  /* 0x0594 - 0x0597 */
+        { 0x0598, 0x0ffe, pcxu_  },  /* 0x0598 - 0x0599 */
+        { 0x059a, 0x0ffe, pcxu   },  /* 0x059a - 0x059b */
+        { 0x059c, 0x0fff, pcxu   },  /* 0x059c - 0x059c */
+        { 0x059d, 0x0fff, pcxu_  },  /* 0x059d - 0x059d */
+        { 0x059e, 0x0fff, pcxt_  },  /* 0x059e - 0x059e */
+        { 0x059f, 0x0fff, pcxu   },  /* 0x059f - 0x059f */
+        { 0x05a0, 0x0ffe, pcxt_  },  /* 0x05a0 - 0x05a1 */
+        { 0x05a2, 0x0ffe, pcxu   },  /* 0x05a2 - 0x05a3 */
+        { 0x05a4, 0x0ffc, pcxu   },  /* 0x05a4 - 0x05a7 */
+        { 0x05a8, 0x0ffc, pcxu   },  /* 0x05a8 - 0x05ab */
+        { 0x05ad, 0x0fff, pcxu_  },  /* 0x05ad - 0x05ad */
+        { 0x05ae, 0x0ffe, pcxu_  },  /* 0x05ae - 0x05af */
+        { 0x05b0, 0x0ffe, pcxu_  },  /* 0x05b0 - 0x05b1 */
+        { 0x05b2, 0x0fff, pcxu_  },  /* 0x05b2 - 0x05b2 */
+        { 0x05b3, 0x0fff, pcxu   },  /* 0x05b3 - 0x05b3 */
+        { 0x05b4, 0x0fff, pcxw   },  /* 0x05b4 - 0x05b4 */
+        { 0x05b5, 0x0fff, pcxu_  },  /* 0x05b5 - 0x05b5 */
+        { 0x05b6, 0x0ffe, pcxu_  },  /* 0x05b6 - 0x05b7 */
+        { 0x05b8, 0x0ffe, pcxu_  },  /* 0x05b8 - 0x05b9 */
+        { 0x05ba, 0x0fff, pcxu_  },  /* 0x05ba - 0x05ba */
+        { 0x05bb, 0x0fff, pcxw   },  /* 0x05bb - 0x05bb */
+        { 0x05bc, 0x0ffc, pcxw   },  /* 0x05bc - 0x05bf */
+        { 0x05c0, 0x0ffc, pcxw   },  /* 0x05c0 - 0x05c3 */
+        { 0x05c4, 0x0ffe, pcxw   },  /* 0x05c4 - 0x05c5 */
+        { 0x05c6, 0x0fff, pcxw   },  /* 0x05c6 - 0x05c6 */
+        { 0x05c7, 0x0fff, pcxw_  },  /* 0x05c7 - 0x05c7 */
+        { 0x05c8, 0x0ffc, pcxw   },  /* 0x05c8 - 0x05cb */
+        { 0x05cc, 0x0ffe, pcxw   },  /* 0x05cc - 0x05cd */
+        { 0x05ce, 0x0ffe, pcxw_  },  /* 0x05ce - 0x05cf */
+        { 0x05d0, 0x0ffc, pcxw_  },  /* 0x05d0 - 0x05d3 */
+        { 0x05d4, 0x0ffe, pcxw_  },  /* 0x05d4 - 0x05d5 */
+        { 0x05d6, 0x0fff, pcxw   },  /* 0x05d6 - 0x05d6 */
+        { 0x05d7, 0x0fff, pcxw_  },  /* 0x05d7 - 0x05d7 */
+        { 0x05d8, 0x0ffc, pcxw_  },  /* 0x05d8 - 0x05db */
+        { 0x05dc, 0x0ffe, pcxw2  },  /* 0x05dc - 0x05dd */
+        { 0x05de, 0x0fff, pcxw_  },  /* 0x05de - 0x05de */
+        { 0x05df, 0x0fff, pcxw2  },  /* 0x05df - 0x05df */
+        { 0x05e0, 0x0ffc, pcxw2  },  /* 0x05e0 - 0x05e3 */
+        { 0x05e4, 0x0fff, pcxw2  },  /* 0x05e4 - 0x05e4 */
+        { 0x05e5, 0x0fff, pcxw_  },  /* 0x05e5 - 0x05e5 */
+        { 0x05e6, 0x0ffe, pcxw2  },  /* 0x05e6 - 0x05e7 */
+        { 0x05e8, 0x0ff8, pcxw2  },  /* 0x05e8 - 0x05ef */
+        { 0x05f0, 0x0ff0, pcxw2  },  /* 0x05f0 - 0x05ff */
+        { 0x0600, 0x0fe0, pcxl   },  /* 0x0600 - 0x061f */
+        { 0x0880, 0x0ff0, mako   },  /* 0x0880 - 0x088f */
+        { 0x0000, 0x0000, pcx    }      /* terminate table */
+};
+                                                                                
+static char *cpu_name_version[][2] = {
+        { "PA7000 (PCX)",     "1.0" },
+        { "PA7000 (PCX-S)",   "1.1a" },
+        { "PA7100 (PCX-T)",   "1.1b" },
+        { "PA7200 (PCX-T')",  "1.1c" },
+        { "PA7100LC (PCX-L)", "1.1d" },
+        { "PA7300LC (PCX-L2)",        "1.1e" },
+        { "PA8000 (PCX-U)",   "2.0" },
+        { "PA8200 (PCX-U+)",  "2.0" },
+        { "PA8500 (PCX-W)",   "2.0" },
+        { "PA8600 (PCX-W+)",  "2.0" },
+        { "PA8700 (PCX-W2)",  "2.0" },
+        { "PA8800 (Mako)",    "2.0" }
+};
+
 static long get_long(const string & path)
 {
   long result = -1;
@@ -132,9 +246,22 @@ static string cpubusinfo(int cpu)
   return string(buffer);
 }
 
+enum cpu_type parisc_get_cpu_type(long hversion)
+{
+  struct hp_cpu_type_mask *ptr;
+                                                                                
+  for (ptr = hp_cpu_type_mask_list; 0 != ptr->mask; ptr++)
+  {
+    if (ptr->model == (hversion & ptr->mask))
+      return ptr->cpu;
+  }
+                                                                                
+  return pcx;     /* not reached: */
+}
+
 static int currentcpu = 0;
                                                                                 
-static hwNode get_device(long hw_type, long sversion)
+static hwNode get_device(long hw_type, long sversion, long hversion)
 {
   hwNode newnode("generic");
 
@@ -145,7 +272,12 @@ static hwNode get_device(long hw_type, long sversion)
       newnode = hwNode(parisc_device_list[i].id, parisc_device_list[i].device_class);
       newnode.setDescription(parisc_device_list[i].description);
       if(hw_type == TP_NPROC)
+      {
+        enum cpu_type cpu = parisc_get_cpu_type(hversion);
         newnode.setBusInfo(cpubusinfo(currentcpu++));
+        newnode.setProduct(cpu_name_version[cpu][0]);
+        newnode.setVersion(cpu_name_version[cpu][1]);
+      }
 
       return newnode;
     }
@@ -231,14 +363,16 @@ static bool scan_device(hwNode & node, string name = "")
     size_t colon = name.rfind(":");
     long hw_type = get_long("hw_type");
     long sversion = get_long("sversion");
-    //long hversion = get_long("hversion");
+    long hversion = get_long("hversion");
     long rev = get_long("rev");
-    hwNode newnode = get_device(hw_type, sversion);
-    char buffer[20];
+    hwNode newnode = get_device(hw_type, sversion, hversion);
 
-    snprintf(buffer, sizeof(buffer), "%ld", rev);
-    if(rev>0)
+    if((rev>0) && (newnode.getVersion() == ""))
+    {
+      char buffer[20];
+      snprintf(buffer, sizeof(buffer), "%ld", rev);
       newnode.setVersion(buffer);
+    }
     if(newnode.getBusInfo()=="")
       newnode.setBusInfo(guessBusInfo(name));
     if(exists("driver"))
