833db629f2275a01ace528c9faa9521188a8239e
lyonel <lyonel@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2007-03-20 14:47:08 +0000

    * code cleanup  * make use of `$BASEDIR` to locate artwork if set
    git-svn-id: http://ezix.org/source/packages/lshw/development@1763 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/gui/stock.c
+++ src/gui/stock.c
@@ -1,4 +1,5 @@
 #include "stock.h"
+#include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
 
@@ -7,69 +8,66 @@ static char *id = "@(#) $Id$";
 static struct StockIcon
 {
   const char *name;
-  const char *dir;
   const char *filename;
 
 } const stock_icons[] =
 {
-  { LSHW_STOCK_AUDIO, ".",   "audio.svg" },
-  { LSHW_STOCK_AMD, ".",   "amd.svg" },
-  { LSHW_STOCK_BATTERY, ".",   "battery.svg" },
-  { LSHW_STOCK_BLUETOOTH, ".",   "bluetooth.svg" },
-  { LSHW_STOCK_BOARD, ".",   "board.svg" },
-  { LSHW_STOCK_CHIP, ".",   "chip.svg" },
-  { LSHW_STOCK_CPU, ".",   "cpu.svg" },
-  { LSHW_STOCK_DESKTOPCOMPUTER, ".",   "desktopcomputer.svg" },
-  { LSHW_STOCK_DISABLED, ".",   "disabled.svg" },
-  { LSHW_STOCK_DISPLAY, ".",   "display.svg" },
-  { LSHW_STOCK_CD, ".",   "cd.svg" },
-  { LSHW_STOCK_DISC, ".",   "disc.svg" },
-  { LSHW_STOCK_FIREWIRE, ".",   "firewire.svg" },
-  { LSHW_STOCK_INTEL, ".",   "intel.svg" },
-  { LSHW_STOCK_LAPTOP, ".",   "laptop.svg" },
-  { LSHW_STOCK_LOGO, ".",   "logo.svg" },
-  { LSHW_STOCK_MEMORY, ".",   "memory.svg" },
-  { LSHW_STOCK_MINI, ".",   "mini.svg" },
-  { LSHW_STOCK_MODEM, ".",   "modem.svg" },
-  { LSHW_STOCK_MOTHERBOARD, ".",   "motherboard.svg" },
-  { LSHW_STOCK_NETWORK, ".",   "network.svg" },
-  { LSHW_STOCK_PARALLEL, ".",   "parallel.svg" },
-  { LSHW_STOCK_POWERMAC, ".",   "powermac.svg" },
-  { LSHW_STOCK_POWERMACG5, ".",   "powermacg5.svg" },
-  { LSHW_STOCK_POWERPC, ".",   "powerpc.svg" },
-  { LSHW_STOCK_PRINTER, ".",   "printer.svg" },
-  { LSHW_STOCK_RADIO, ".",   "radio.svg" },
-  { LSHW_STOCK_MD, ".",   "md.svg" },
-  { LSHW_STOCK_SCSI, ".",   "scsi.svg" },
-  { LSHW_STOCK_SERIAL, ".",   "serial.svg" },
-  { LSHW_STOCK_TOWERCOMPUTER, ".",   "towercomputer.svg" },
-  { LSHW_STOCK_USB, ".",   "usb.svg" },
-  { LSHW_STOCK_WIFI, ".",   "wifi.svg" },
+  { LSHW_STOCK_AUDIO, "audio.svg" },
+  { LSHW_STOCK_AMD, "amd.svg" },
+  { LSHW_STOCK_BATTERY, "battery.svg" },
+  { LSHW_STOCK_BLUETOOTH, "bluetooth.svg" },
+  { LSHW_STOCK_BOARD, "board.svg" },
+  { LSHW_STOCK_CHIP, "chip.svg" },
+  { LSHW_STOCK_CPU, "cpu.svg" },
+  { LSHW_STOCK_DESKTOPCOMPUTER, "desktopcomputer.svg" },
+  { LSHW_STOCK_DISABLED, "disabled.svg" },
+  { LSHW_STOCK_DISPLAY, "display.svg" },
+  { LSHW_STOCK_CD, "cd.svg" },
+  { LSHW_STOCK_DISC, "disc.svg" },
+  { LSHW_STOCK_FIREWIRE, "firewire.svg" },
+  { LSHW_STOCK_INTEL, "intel.svg" },
+  { LSHW_STOCK_LAPTOP, "laptop.svg" },
+  { LSHW_STOCK_LOGO, "logo.svg" },
+  { LSHW_STOCK_MEMORY, "memory.svg" },
+  { LSHW_STOCK_MINI, "mini.svg" },
+  { LSHW_STOCK_MODEM, "modem.svg" },
+  { LSHW_STOCK_MOTHERBOARD, "motherboard.svg" },
+  { LSHW_STOCK_NETWORK, "network.svg" },
+  { LSHW_STOCK_PARALLEL, "parallel.svg" },
+  { LSHW_STOCK_POWERMAC, "powermac.svg" },
+  { LSHW_STOCK_POWERMACG5, "powermacg5.svg" },
+  { LSHW_STOCK_POWERPC, "powerpc.svg" },
+  { LSHW_STOCK_PRINTER, "printer.svg" },
+  { LSHW_STOCK_RADIO, "radio.svg" },
+  { LSHW_STOCK_MD, "md.svg" },
+  { LSHW_STOCK_SCSI, "scsi.svg" },
+  { LSHW_STOCK_SERIAL, "serial.svg" },
+  { LSHW_STOCK_TOWERCOMPUTER, "towercomputer.svg" },
+  { LSHW_STOCK_USB, "usb.svg" },
+  { LSHW_STOCK_WIFI, "wifi.svg" },
 };
 
 static gchar *
-find_file(const char *dir, const char *base)
+find_file(const char *base)
 {
   char *filename;
+  char *basedir;
 
   if (base == NULL)
     return NULL;
 
-  if (!strcmp(dir, "lshw"))
-    filename = g_build_filename(DATADIR, "lshw", "artwork", base, NULL);
+  if(basedir = getenv("BASEDIR"))
+    filename = g_build_filename(basedir, "artwork", base, NULL);
   else
-  {
-    filename = g_build_filename(DATADIR, "lshw", "artwork", dir, base, NULL);
-  }
+    filename = g_build_filename(DATADIR, "lshw", "artwork", base, NULL);
 
   if (!g_file_test(filename, G_FILE_TEST_EXISTS))
   {
     g_free(filename);
-
     return NULL;
   }
-
-  return filename;
+  else
+    return filename;
 }
 
 
@@ -101,15 +99,7 @@ lshw_gtk_stock_init(void)
     GtkIconSet *iconset;
     gchar *filename;
 
-    if (stock_icons[i].dir == NULL)
-    {
-/* GTK+ Stock icon */
-      iconset = gtk_style_lookup_icon_set(gtk_widget_get_style(win),
-        stock_icons[i].filename);
-    }
-    else
-    {
-      filename = find_file(stock_icons[i].dir, stock_icons[i].filename);
+      filename = find_file(stock_icons[i].filename);
 
       if (filename == NULL)
         continue;
@@ -121,7 +111,6 @@ lshw_gtk_stock_init(void)
       iconset = gtk_icon_set_new_from_pixbuf(pixbuf);
 
       g_object_unref(G_OBJECT(pixbuf));
-    }
 
     gtk_icon_factory_add(icon_factory, stock_icons[i].name, iconset);
 
