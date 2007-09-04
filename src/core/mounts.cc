74f8eae628858844a02610932d9927f38ceb455c
lyonel <lyonel@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2007-09-04 09:43:54 +0000

    commit missing file
    git-svn-id: http://ezix.org/source/packages/lshw/development@1879 811e2811-9fd9-0310-a116-b6e8ac943c8b

---
+++ src/core/mounts.cc
@@ -0,0 +1,98 @@
+/*
+ * mounts.cc
+ *
+ *
+ */
+
+#include "version.h"
+#include "mounts.h"
+#include "osutils.h"
+#include <vector>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+__ID("@(#) $Id: mounts.cc 1352 2006-05-27 23:54:13Z ezix $");
+
+#define MOUNTS "/proc/mounts"
+
+static bool has_device(const string & dev, hwNode & n)
+{
+  string devid = get_devid(dev);
+  vector <string> lnames;
+  size_t i;
+
+  if(devid == "")
+    return false;
+
+  if(get_devid(dev) == n.getDev())
+    return true;
+
+  lnames = n.getLogicalNames();
+  for(i=0; i<lnames.size(); i++)
+  {
+    if(get_devid(lnames[i]) == devid)
+      return true;
+  }
+  return false;
+}
+
+static void update_mount_status(hwNode & n, const vector <string> & mount)
+{
+  unsigned i;
+
+  if(has_device(mount[0], n))
+  {
+    n.setConfig("mounted", "true");
+    n.setLogicalName(mount[1]);		// mountpoint
+    n.setConfig("mount.fstype", mount[2]);
+    n.setConfig("mount.options", mount[3]);
+  }
+
+  for(i=0; i<n.countChildren(); i++)
+    update_mount_status(*n.getChild(i), mount);
+}
+
+static bool process_mount(const string & s, hwNode & n)
+{
+  vector <string> mount;
+  struct stat buf;
+
+  // entries' format is
+  // devicenode mountpoint fstype mountoptions dumpfrequency pass
+  if(splitlines(s, mount, ' ') != 6)
+  {
+    mount.clear();
+    if(splitlines(s, mount, '\t') != 6)
+      return false;
+  }
+
+  if(mount[0][0] != '/')	// devicenode isn't a full path
+    return false;
+
+  if(stat(mount[0].c_str(), &buf) != 0)
+    return false;
+
+  if(!S_ISBLK(buf.st_mode))	// we're only interested in block devices
+    return false;
+
+  update_mount_status(n, mount);
+
+  return true;
+}
+
+bool scan_mounts(hwNode & n)
+{
+  vector <string> mounts;
+  size_t i;
+
+  if(!loadfile(MOUNTS, mounts))
+    return false;
+
+  for(i=0; i<mounts.size(); i++)
+    process_mount(mounts[i], n);
+
+  return true;
+}
