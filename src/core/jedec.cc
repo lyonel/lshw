194b8c70afaecbab454b2c37ab5c0129a4b928cc
lyonel <lyonel@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2010-03-14 22:07:18 +0000

    added more JEDEC ids
    git-svn-id: http://ezix.org/source/packages/lshw/development@2149 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/jedec.cc
+++ src/core/jedec.cc
@@ -14,6 +14,31 @@ static const char * jedec_id[] = {
 	"8002",	"AMI",
 	"04",	"Fujitsu",
 	"8004",	"Fujitsu",
+	"0500",	"Elpida",
+	"07",	"Hitachi",
+	"8007",	"Hitachi",
+	"08",	"Inmos",
+	"8008",	"Inmos",
+	"0B",	"Intersil",
+	"800B",	"Intersil",
+	"0D",	"Mostek",
+	"800D",	"Mostek",
+	"0E",	"Freescale (Motorola)",
+	"800E",	"Freescale (Motorola)",
+	"10",	"NEC",
+	"8010",	"NEC",
+	"13",	"Conexant (Rockwell)",
+	"8013",	"Conexant (Rockwell)",
+	"15",	"NXP (Philips Semi, Signetics)",
+	"8015",	"NXP (Philips Semi, Signetics)",
+	"16",	"Synertek",
+	"8016",	"Synertek",
+	"19",	"Xicor",
+	"8019",	"Xicor",
+	"1A",	"Zilog",
+	"801A",	"Zilog",
+	"1C",	"Mitsubishi",
+	"801C",	"Mitsubishi",
 	"C1",	"Infineon (Siemens)",
 	"80C1",	"Infineon (Siemens)",
 	"7F7F7F7F7F51",	"Infineon (Siemens)",
@@ -31,7 +56,7 @@ string jedec_resolve(const string & s)
     result.erase(0, 2);
 
   for(int i=0; jedec_id[i]; i+=2) {
-    if(matches(result, jedec_id[i], REG_ICASE)) return string(jedec_id[i+1]);
+    if(matches(result, "^" + string(jedec_id[i]), REG_ICASE)) return string(jedec_id[i+1]);
   }
 
   return s;
