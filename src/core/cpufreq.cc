37c458b3fd59fd7066c043e43e8bc5172f6092c2
lyonel <lyonel@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2012-01-19 12:04:26 +0000

    fix reporting of CPU maximum frequency (cf. #552 and #516)
    git-svn-id: http://ezix.org/source/packages/lshw/development@2470 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/cpufreq.cc
+++ src/core/cpufreq.cc
@@ -69,7 +69,7 @@ bool scan_cpufreq(hwNode & node)
       cur = 1000*(unsigned long long)get_long("scaling_cur_freq");
       cpu->addCapability("cpufreq", "CPU Frequency scaling");
       if(cur) cpu->setSize(cur);
-      if(max>cpu->getCapacity()) cpu->setCapacity(cur);
+      if(max>cpu->getCapacity()) cpu->setCapacity(max);
       popd();
     }
     i++;
