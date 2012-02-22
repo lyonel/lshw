b43a38869eacb9c0245b1ffe43f842ebc9a85270
lyonel <lyonel@811e2811-9fd9-0310-a116-b6e8ac943c8b>
2012-02-22 13:12:49 +0000

    slight code clean-up
    git-svn-id: http://ezix.org/source/packages/lshw/development@2489 811e2811-9fd9-0310-a116-b6e8ac943c8b

--- src/core/fat.cc
+++ src/core/fat.cc
@@ -4,8 +4,8 @@
  * CAUTION: this version is severely brain-damaged, don't blame Kay
  *
  * for instance, it will only detect the volume label on FAT32 if it
- * 	is declared within the first cluster of the root directory
- * 	(i.e. usually within the first 128 files)
+ *      is declared within the first cluster of the root directory
+ *      (i.e. usually within the first 128 files)
  *
  * original copyright follows:
  *
@@ -32,78 +32,77 @@
 #include <string.h>
 
 /* linux/msdos_fs.h says: */
-#define FAT12_MAX			0xff4
-#define FAT16_MAX			0xfff4
-#define FAT32_MAX			0x0ffffff6
+#define FAT12_MAX                       0xff4
+#define FAT16_MAX                       0xfff4
+#define FAT32_MAX                       0x0ffffff6
 
-#define FAT_ATTR_VOLUME_ID		0x08
-#define FAT_ATTR_DIR			0x10
-#define FAT_ATTR_LONG_NAME		0x0f
-#define FAT_ATTR_MASK			0x3f
-#define FAT_ENTRY_FREE			0xe5
+#define FAT_ATTR_VOLUME_ID              0x08
+#define FAT_ATTR_DIR                    0x10
+#define FAT_ATTR_LONG_NAME              0x0f
+#define FAT_ATTR_MASK                   0x3f
+#define FAT_ENTRY_FREE                  0xe5
 
 struct vfat_super_block {
-	uint8_t		boot_jump[3];
-	uint8_t		sysid[8];
-	uint16_t	sector_size_bytes;
-	uint8_t		sectors_per_cluster;
-	uint16_t	reserved_sct;
-	uint8_t		fats;
-	uint16_t	dir_entries;
-	uint16_t	sectors;
-	uint8_t		media;
-	uint16_t	fat_length;
-	uint16_t	secs_track;
-	uint16_t	heads;
-	uint32_t	hidden;
-	uint32_t	total_sect;
+	uint8_t boot_jump[3];
+	uint8_t sysid[8];
+	uint16_t sector_size_bytes;
+	uint8_t sectors_per_cluster;
+	uint16_t reserved_sct;
+	uint8_t fats;
+	uint16_t dir_entries;
+	uint16_t sectors;
+	uint8_t media;
+	uint16_t fat_length;
+	uint16_t secs_track;
+	uint16_t heads;
+	uint32_t hidden;
+	uint32_t total_sect;
 	union {
 		struct fat_super_block {
-			uint8_t		unknown[3];
-			uint8_t		serno[4];
-			uint8_t		label[11];
-			uint8_t		magic[8];
-			uint8_t		dummy2[192];
-			uint8_t		pmagic[2];
+			uint8_t unknown[3];
+			uint8_t serno[4];
+			uint8_t label[11];
+			uint8_t magic[8];
+			uint8_t dummy2[192];
+			uint8_t pmagic[2];
 		} __attribute__((__packed__)) fat;
 		struct fat32_super_block {
-			uint32_t	fat32_length;
-			uint16_t	flags;
-			uint8_t		version[2];
-			uint32_t	root_cluster;
-			uint16_t	insfo_sector;
-			uint16_t	backup_boot;
-			uint16_t	reserved2[6];
-			uint8_t		unknown[3];
-			uint8_t		serno[4];
-			uint8_t		label[11];
-			uint8_t		magic[8];
-			uint8_t		dummy2[164];
-			uint8_t		pmagic[2];
+			uint32_t fat32_length;
+			uint16_t flags;
+			uint8_t version[2];
+			uint32_t root_cluster;
+			uint16_t insfo_sector;
+			uint16_t backup_boot;
+			uint16_t reserved2[6];
+			uint8_t unknown[3];
+			uint8_t serno[4];
+			uint8_t label[11];
+			uint8_t magic[8];
+			uint8_t dummy2[164];
+			uint8_t pmagic[2];
 		} __attribute__((__packed__)) fat32;
 	} __attribute__((__packed__)) type;
 } __attribute__((__packed__));
 
 struct vfat_dir_entry {
-	uint8_t		name[11];
-	uint8_t		attr;
-	uint16_t	time_creat;
-	uint16_t	date_creat;
-	uint16_t	time_acc;
-	uint16_t	date_acc;
-	uint16_t	cluster_high;
-	uint16_t	time_write;
-	uint16_t	date_write;
-	uint16_t	cluster_low;
-	uint32_t	size;
+	uint8_t name[11];
+	uint8_t attr;
+	uint16_t time_creat;
+	uint16_t date_creat;
+	uint16_t time_acc;
+	uint16_t date_acc;
+	uint16_t cluster_high;
+	uint16_t time_write;
+	uint16_t date_write;
+	uint16_t cluster_low;
+	uint32_t size;
 } __attribute__((__packed__));
 
 static uint8_t *get_attr_volume_id(struct vfat_dir_entry *dir, int count)
 {
-	for (;--count >= 0; dir++) {
+	for (; --count >= 0; dir++) {
 		/* end marker */
 		if (dir->name[0] == 0x00) {
-			//dbg("end of dir");
 			break;
 		}
 
@@ -120,11 +119,9 @@ static uint8_t *get_attr_volume_id(struct vfat_dir_entry *dir, int count)
 			if (dir->cluster_high != 0 || dir->cluster_low != 0)
 				continue;
 
-			//dbg("found ATTR_VOLUME_ID id in root dir");
 			return dir->name;
 		}
 
-		//dbg("skip dir entry");
 	}
 
 	return NULL;
@@ -149,7 +146,6 @@ bool scan_fat(hwNode & n, source & id)
 	uint8_t *label = NULL;
 	uint32_t next_cluster;
 
-	//dbg("probing at offset 0x%llx", (unsigned long long) fat_partition_off);
 
 	if(!readlogicalblocks(id, &vs, 0, 1))
 		return false;
@@ -182,7 +178,7 @@ bool scan_fat(hwNode & n, source & id)
 
 	/* boot jump address check */
 	if ((vs.boot_jump[0] != 0xeb || vs.boot_jump[2] != 0x90) &&
-	     vs.boot_jump[0] != 0xe9)
+	    vs.boot_jump[0] != 0xe9)
 		return false;
 
 	/* heads check */
@@ -202,39 +198,32 @@ bool scan_fat(hwNode & n, source & id)
 	if (vs.fats != 2)
 		return false;
 
- valid:
+valid:
 	/* sector size check */
 	sector_size_bytes = le_short(&vs.sector_size_bytes);
 	if (sector_size_bytes != 0x200 && sector_size_bytes != 0x400 &&
 	    sector_size_bytes != 0x800 && sector_size_bytes != 0x1000)
 		return false;
 
-	//dbg("sector_size_bytes 0x%x", sector_size_bytes);
-	//dbg("sectors_per_cluster 0x%x", vs.sectors_per_cluster);
 	id.blocksize = sector_size_bytes;
 
 	reserved_sct = le_short(&vs.reserved_sct);
-	//dbg("reserved_sct 0x%x", reserved_sct);
 
 	sect_count = le_short(&vs.sectors);
 	if (sect_count == 0)
 		sect_count = le_long(&vs.total_sect);
-	//dbg("sect_count 0x%x", sect_count);
 
 	fat_size_sct = le_short(&vs.fat_length);
 	if (fat_size_sct == 0)
 		fat_size_sct = le_long(&vs.type.fat32.fat32_length);
 	fat_size_sct *= vs.fats;
-	//dbg("fat_size_sct 0x%x", fat_size_sct);
 
 	dir_entries = le_short(&vs.dir_entries);
 	dir_size_sct = ((dir_entries * sizeof(struct vfat_dir_entry)) +
-			(sector_size_bytes-1)) / sector_size_bytes;
-	//dbg("dir_size_sct 0x%x", dir_size_sct);
+	                (sector_size_bytes-1)) / sector_size_bytes;
 
 	cluster_count = sect_count - (reserved_sct + fat_size_sct + dir_size_sct);
 	cluster_count /= vs.sectors_per_cluster;
-	//dbg("cluster_count 0x%x", cluster_count);
 
 //	if (cluster_count < FAT12_MAX) {
 //		strcpy(id->type_version, "FAT12");
@@ -249,8 +238,6 @@ bool scan_fat(hwNode & n, source & id)
 
 	/* the label may be an attribute in the root directory */
 	root_start_sect = reserved_sct + fat_size_sct;
-	//dbg("root dir start 0x%llx", (unsigned long long) root_start_sect);
-	//dbg("expected entries 0x%x", dir_entries);
 
 	buf_size = dir_entries * sizeof(struct vfat_dir_entry);
 	buf = (uint8_t*)malloc(buf_size);
@@ -266,7 +253,7 @@ bool scan_fat(hwNode & n, source & id)
 	}
 	goto found;
 
- fat32:
+fat32:
 	/* FAT32 root dir is a cluster chain like any other directory */
 	/* FIXME: we actually don't bother checking the whole chain */
 	/* --> the volume label is assumed to be within the first cluster (usually 128 entries)*/
@@ -274,7 +261,7 @@ bool scan_fat(hwNode & n, source & id)
 	buf = (uint8_t*)malloc(buf_size);
 	root_cluster = le_long(&vs.type.fat32.root_cluster);
 	start_data_sct = reserved_sct + fat_size_sct;
-        id.blocksize = sector_size_bytes;
+	id.blocksize = sector_size_bytes;
 
 	next_cluster = root_cluster;
 	{
@@ -282,10 +269,8 @@ bool scan_fat(hwNode & n, source & id)
 		uint64_t next_off;
 		int count;
 
-		//dbg("next_cluster 0x%x", (unsigned)next_cluster);
 		next_off_sct = (next_cluster - 2) * vs.sectors_per_cluster;
 		next_off = start_data_sct + next_off_sct;
-		//dbg("cluster offset 0x%llx", (unsigned long long) next_off);
 
 		/* get cluster */
 		if(!readlogicalblocks(id, buf, next_off, vs.sectors_per_cluster))
@@ -293,7 +278,6 @@ bool scan_fat(hwNode & n, source & id)
 
 		dir = (struct vfat_dir_entry*) buf;
 		count = buf_size / sizeof(struct vfat_dir_entry);
-		//dbg("expected entries 0x%x", count);
 
 		label = get_attr_volume_id(dir, count);
 	}
@@ -304,7 +288,7 @@ bool scan_fat(hwNode & n, source & id)
 		n.setConfig("label", hw::strip(string((char*)vs.type.fat.label, 11)));
 	}
 
- found:
+found:
 	return true;
 }
 
