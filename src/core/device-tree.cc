68885a19bd7b16a8cddf2730e7ccd4a68ea8fb33
Chandni Verma <chandni@linux.vnet.ibm.com>
2016-03-31 17:54:14 +0530

    Display CPU nodes before memory
    We do this because CPU is displayed before any other child-nodes of core
    on other platforms not depending on device-tree as well.
    
    We do this for powernv as well as other platforms relying on device-tree.
    
    Signed-off-by: Chandni Verma <chandni@linux.vnet.ibm.com>

--- src/core/device-tree.cc
+++ src/core/device-tree.cc
@@ -1042,8 +1042,8 @@ bool scan_device_tree(hwNode & n)
     {
       core->addHint("icon", string("board"));
       scan_devtree_root(*core);
-      scan_devtree_memory_powernv(*core);
       scan_devtree_cpu_power(*core);
+      scan_devtree_memory_powernv(*core);
       n.addCapability("powernv", "Non-virtualized");
       n.addCapability("opal", "OPAL firmware");
     }
@@ -1092,8 +1092,6 @@ bool scan_device_tree(hwNode & n)
     {
       core->addHint("icon", string("board"));
       scan_devtree_root(*core);
-      scan_devtree_bootrom(*core);
-      scan_devtree_memory(*core);
       if (exists(DEVICETREE "/ibm,lpar-capable"))
       {
         n.setDescription("pSeries LPAR");
@@ -1101,6 +1099,8 @@ bool scan_device_tree(hwNode & n)
       }
       else
         scan_devtree_cpu(*core);
+      scan_devtree_bootrom(*core);
+      scan_devtree_memory(*core);
     }
   }
 
