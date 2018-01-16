lyonel/lshw

commit f95aa917a84a8ee74ce79e9b4f9e198d21a2e4d9

merge patches from beaker-fork
cf. https://github.com/lyonel/lshw/pull/31
seems ok.

src/core/device-tree.cc
@@ -981,8 +981,9 @@ static void add_memory_bank(string name, string path, hwNode & core)
   if(!memory)
     memory = core.addChild(hwNode("memory", hw::memory));
 
-  pushd(path + name);
-  if(name.substr(0, 7) == "ms-dimm")
+  pushd(path + "/" + name);
+  if(name.substr(0, 7) == "ms-dimm" ||
+     name.substr(0, 18) == "IBM,memory-module@")
   {
     hwNode bank("bank", hw::memory);
     bank.claim(true);
@@ -999,12 +1000,24 @@ static void add_memory_bank(string name, string path, hwNode & core)
     if(product != "")
       bank.setProduct(hw::strip(product));
 
+    string description = "DIMM";
+    string package = hw::strip(get_string("ibm,mem-package"));
+    if (!package.empty())
+      description = package;
+    string memtype = hw::strip(get_string("ibm,mem-type"));
+    if (!memtype.empty())
+      description += " " + memtype;
     if(exists("description"))
-      bank.setDescription(hw::strip(get_string("description")));
+      description = hw::strip(get_string("description"));
+    bank.setDescription(description);
+
     if(exists("ibm,loc-code"))
       bank.setSlot(hw::strip(get_string("ibm,loc-code")));
-    if(unsigned long size = get_number("size"))
-      bank.setSize(size*1024*1024);
+    unsigned long size = get_number("size") * 1024 * 1024;
+    if (exists("ibm,size"))
+      size = get_u32("ibm,size");
+    if (size > 0)
+      bank.setSize(size);
 
     memory->addChild(bank);
   } else if(name.substr(0, 4) == "dimm") {
@@ -1026,7 +1039,7 @@ static void add_memory_bank(string name, string path, hwNode & core)
 
   for (int i = 0; i < n; i++)
   {
-    add_memory_bank(dirlist[i]->d_name, path + name + "/", core);
+    add_memory_bank(dirlist[i]->d_name, path + "/" + name, core);
     free(dirlist[i]);
   }
   free(dirlist);
@@ -1056,6 +1069,30 @@ static void scan_devtree_memory_powernv(hwNode & core)
 }
 
 
+// older POWER hardware
+static void scan_devtree_memory_ibm(hwNode & core)
+{
+  struct dirent **namelist;
+  pushd(DEVICETREE);
+  int n = scandir(".", &namelist, selectdir, alphasort);
+  popd();
+
+  if (n < 0)
+    return;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (strncmp(namelist[i]->d_name, "memory-controller@", 18) == 0)
+    {
+      add_memory_bank(namelist[i]->d_name, DEVICETREE, core);
+    }
+    free(namelist[i]);
+  }
+  free(namelist);
+}
+
+
+// Apple and ARM
 static void scan_devtree_memory(hwNode & core)
 {
   int currentmc = -1;                             // current memory controller
@@ -1233,6 +1270,64 @@ static bool get_apple_model(hwNode & n)
   return false;
 }
 
+struct ibm_model_def {
+  const char *model;
+  const char *modelname;
+  const char *icon;
+  const char *chassis; // matches DMI chassis types
+};
+struct ibm_model_def ibm_model_defs[] =
+{
+  {"0200", "BladeCenter QS20", "", "blade"},
+  {"0792", "BladeCenter QS21", "", "blade"},
+  {"6778", "BladeCenter JS21", "", "blade"},
+  {"6779", "BladeCenter JS21", "", "blade"},
+  {"7047-185", "IntelliStation POWER 185", "towercomputer", "tower"},
+  {"7988", "BladeCenter JS21", "", "blade"},
+  {"8202", "Power 720 Express", "", "rackmount"},
+  {"8205", "Power 740 Express", "", "rackmount"},
+  {"8231-E1C", "Power 710 Express", "", "rackmount"},
+  {"8231-E1D", "Power 710 Express", "", "rackmount"},
+  {"8231-E2C", "Power 730 Express", "", "rackmount"},
+  {"8231-E2D", "Power 730 Express", "", "rackmount"},
+  {"8233-E8B", "Power 750 Express", "", "rackmount"},
+  {"8236-E8C", "Power 755", "", "rackmount"},
+  {"8286-41A", "Power Systems S814", "", "rackmount"},
+  {"8286-42A", "Power Systems S824", "", "rackmount"},
+  {"8406-70Y", "Power PS700", "", "rackmount"},
+  {"8406-71Y", "BladeCenter PS702", "", "blade"},
+  {"8842", "BladeCenter JS20", "", "blade"},
+  {"8844", "BladeCenter JS21", "", "blade"},
+  {"9111-285", "IntelliStation POWER 285", "towercomputer", "tower"},
+  {"9112-265", "IntelliStation POWER 265", "towercomputer", "tower"},
+  {"9114-275", "IntelliStation POWER 275", "towercomputer", "tower"},
+  {"9123", "eServer OpenPower 710", "", "rackmount"},
+  {"9124", "eServer OpenPower 720", "", "rackmount"},
+};
+
+static void get_ibm_model(hwNode & n)
+{
+  string product = n.getProduct();
+  if (product.empty())
+    return;
+  if (product.compare(0, 4, "IBM,") != 0)
+    return;
+
+  n.setVendor("IBM");
+  string machinetype = product.substr(4, 4);
+  string model = product.substr(4);
+
+  for (size_t i = 0; i < sizeof(ibm_model_defs) / sizeof(ibm_model_def); i ++)
+  {
+    if (ibm_model_defs[i].model == machinetype || ibm_model_defs[i].model == model)
+    {
+      n.setProduct(ibm_model_defs[i].modelname);
+      n.addHint("icon", string(ibm_model_defs[i].icon));
+      n.setConfig("chassis", ibm_model_defs[i].chassis);
+      return;
+    }
+  }
+}
 
 static void fix_serial_number(hwNode & n)
 {
@@ -1265,10 +1360,11 @@ bool scan_device_tree(hwNode & n)
     n.setSerial(get_string(DEVICETREE "/system-id"));
   fix_serial_number(n);
 
+  n.setVendor(get_string(DEVICETREE "/copyright", n.getVendor()));
+  get_apple_model(n);
+  get_ibm_model(n);
   if (matches(get_string(DEVICETREE "/compatible"), "^ibm,powernv"))
   {
-    n.setVendor(get_string(DEVICETREE "/vendor", "IBM"));
-
     if (exists(DEVICETREE "/model-name"))
       n.setProduct(n.getProduct() + " (" +
 		   hw::strip(get_string(DEVICETREE "/model-name")) + ")");
@@ -1279,6 +1375,7 @@ bool scan_device_tree(hwNode & n)
       core->addHint("icon", string("board"));
       scan_devtree_root(*core);
       scan_devtree_cpu_power(*core);
+      scan_devtree_memory_ibm(*core);
       scan_devtree_memory_powernv(*core);
       scan_devtree_firmware_powernv(*core);
       n.addCapability("powernv", "Non-virtualized");
@@ -1323,8 +1420,6 @@ bool scan_device_tree(hwNode & n)
   }
   else
   {
-    n.setVendor(get_string(DEVICETREE "/copyright", n.getVendor()));
-    get_apple_model(n);
     if (core)
     {
       core->addHint("icon", string("board"));

src/Makefile
@@ -39,7 +39,7 @@ export CXXFLAGS
 export LIBS
 export LDFLAGS
 
-DATAFILES = pci.ids usb.ids oui.txt manuf.txt
+DATAFILES = pci.ids usb.ids oui.txt manuf.txt pnp.ids pnpid.txt
 
 all: $(PACKAGENAME) $(PACKAGENAME).1 $(DATAFILES)
 
@@ -92,6 +92,12 @@ oui.txt:
 
 manuf.txt:
 	wget -O $@ http://anonsvn.wireshark.org/wireshark/trunk/manuf
+
+pnp.ids:
+	wget https://git.fedorahosted.org/cgit/hwdata.git/plain/pnp.ids
+
+pnpid.txt:
+	wget http://www-pc.uni-regensburg.de/hardware/TECHNIK/PCI_PNP/pnpid.txt
 
 install: all
 	$(INSTALL) -d -m 0755 $(DESTDIR)/$(SBINDIR)

src/core/Makefile
@@ -8,7 +8,7 @@ LDFLAGS=
 LDSTATIC=
 LIBS=
 
-OBJS = hw.o main.o print.o mem.o dmi.o device-tree.o cpuinfo.o osutils.o pci.o version.o cpuid.o ide.o cdrom.o pcmcia-legacy.o scsi.o dasd.o disk.o spd.o network.o isapnp.o pnp.o fb.o options.o usb.o sysfs.o display.o heuristics.o parisc.o cpufreq.o partitions.o blockio.o lvm.o ideraid.o pcmcia.o volumes.o mounts.o smp.o abi.o jedec.o dump.o fat.o virtio.o
+OBJS = hw.o main.o print.o mem.o dmi.o device-tree.o cpuinfo.o osutils.o pci.o version.o cpuid.o ide.o cdrom.o pcmcia-legacy.o scsi.o s390.o disk.o spd.o network.o isapnp.o pnp.o fb.o options.o usb.o sysfs.o display.o heuristics.o parisc.o cpufreq.o partitions.o blockio.o lvm.o ideraid.o pcmcia.o volumes.o mounts.o smp.o abi.o jedec.o dump.o fat.o virtio.o vio.o
 ifeq ($(SQLITE), 1)
 	OBJS+= db.o
 endif
@@ -37,9 +37,9 @@ hw.o: hw.h osutils.h version.h config.h options.h heuristics.h
 main.o: hw.h print.h version.h options.h mem.h dmi.h cpuinfo.h cpuid.h
 main.o: device-tree.h pci.h pcmcia.h pcmcia-legacy.h ide.h scsi.h spd.h
 main.o: network.h isapnp.h fb.h usb.h sysfs.h display.h parisc.h cpufreq.h
-main.o: ideraid.h mounts.h smp.h abi.h dasd.h virtio.h
+main.o: ideraid.h mounts.h smp.h abi.h s390.h virtio.h pnp.h vio.h
 print.o: print.h hw.h options.h version.h osutils.h config.h
-mem.o: version.h config.h mem.h hw.h
+mem.o: version.h config.h mem.h hw.h sysfs.h
 dmi.o: version.h config.h dmi.h hw.h osutils.h
 device-tree.o: version.h device-tree.h hw.h osutils.h
 cpuinfo.o: version.h cpuinfo.h hw.h osutils.h
@@ -56,7 +56,7 @@ spd.o: version.h spd.h hw.h osutils.h
 network.o: version.h config.h network.h hw.h osutils.h sysfs.h options.h
 network.o: heuristics.h
 isapnp.o: version.h isapnp.h hw.h pnp.h
-pnp.o: version.h pnp.h hw.h
+pnp.o: version.h pnp.h hw.h sysfs.h osutils.h
 fb.o: version.h fb.h hw.h
 options.o: version.h options.h osutils.h
 usb.o: version.h usb.h hw.h osutils.h heuristics.h options.h
@@ -75,5 +75,6 @@ mounts.o: version.h mounts.h hw.h osutils.h
 smp.o: version.h smp.h hw.h osutils.h
 abi.o: version.h abi.h hw.h osutils.h
 jedec.o: jedec.h
-dasd.o: dasd.h disk.h hw.h osutils.h
+s390.o: hw.h sysfs.h disk.h s390.h
 virtio.o: version.h hw.h sysfs.h disk.h virtio.h
+vio.o: version.h hw.h sysfs.h vio.h

src/core/cpuinfo.cc
@@ -62,7 +62,6 @@ int n = 0)
 }
 
 
-#ifdef __powerpc__
 static void cpuinfo_ppc_ibm(hwNode & node,
 			    const string & description, const string & version)
 {
@@ -103,9 +102,7 @@ string value)
     }
   }
 }
-#endif
 
-#ifdef __s390x__
 static vector <string> s390x_features;
 static string s390x_vendor;
 static void cpuinfo_s390x(hwNode & node,
@@ -139,7 +136,7 @@ string value)
       cpu->claim(true);
       cpu->setVendor(s390x_vendor);
 
-      for(int i=0; i < s390x_features.size(); i++)
+      for(size_t i=0; i < s390x_features.size(); i++)
         cpu->addCapability(s390x_features[i]);
       /* many thanks to Martin Schwidefsky for communicating the descriptions
          of the feature flags
@@ -157,9 +154,7 @@ string value)
       cpu->describeCapability("te", "transactional/constraint transactional execution facilities");
     }
 }
-#endif
 
-#ifdef __arm__
 static void cpuinfo_arm(hwNode & node,
                         string id,
                         string value)
@@ -218,9 +213,7 @@ static void cpuinfo_arm(hwNode & node,
       cpu->describeCapability("evtstrm", "Unknown");
     }
 }
-#endif
 
-#ifdef __aarch64__
 static vector <string> aarch64_features;
 static string aarch64_processor_name;
 static void cpuinfo_aarch64(hwNode & node,
@@ -265,7 +258,7 @@ static void cpuinfo_aarch64(hwNode & node,
               if (node.getDescription() == "")
                 node.setDescription(aarch64_processor_name);
               cpu->claim(true);
-              for(int i=0; i < aarch64_features.size(); i++)
+              for(size_t i=0; i < aarch64_features.size(); i++)
                 {
                   cpu->addCapability(aarch64_features[i]);
                   cpu->describeCapability("fp", "Floating point instructions");
@@ -281,15 +274,11 @@ static void cpuinfo_aarch64(hwNode & node,
         }
     }
 }
-#endif
 
-#ifdef __ia64__
 static void cpuinfo_ia64(hwNode & node,
 string id,
 string value)
 {
-  unsigned long long frequency = 0;
-  int i;
 
   if (id == "processor")
     currentcpu++;
@@ -335,9 +324,7 @@ string value)
     }
   }
 }
-#endif
 
-#ifdef __hppa__
 static void cpuinfo_hppa(hwNode & node,
 string id,
 string value)
@@ -371,9 +358,7 @@ string value)
     }
   }
 }
-#endif
 
-#ifdef __alpha__
 static void cpuinfo_alpha(hwNode & node,
 string id,
 string value)
@@ -431,9 +416,7 @@ string value)
       mycpu->enable();
   }
 }
-#endif
 
-#if defined(__i386__) || defined(__x86_64__)
 static void cpuinfo_x86(hwNode & node,
 string id,
 string value)
@@ -577,7 +560,6 @@ string value)
     if(node.getWidth()==0) node.setWidth(cpu->getWidth());
   }
 }
-#endif
 
 bool scan_cpuinfo(hwNode & n)
 {
@@ -599,6 +581,7 @@ bool scan_cpuinfo(hwNode & n)
     size_t count;
     string cpuinfo_str = "";
     string description = "", version = "";
+    string plat = platform();
 
     while ((count = read(cpuinfo, buffer, sizeof(buffer))) > 0)
     {
@@ -624,48 +607,53 @@ bool scan_cpuinfo(hwNode & n)
         id = hw::strip(cpuinfo_lines[i].substr(0, pos));
         value = hw::strip(cpuinfo_lines[i].substr(pos + 1));
 
-#if defined(__i386__) || defined(__x86_64__)
-        cpuinfo_x86(n, id, value);
-#endif
-#ifdef __powerpc__
-
-        // All cores have same product name and version on power systems
-        if (is_system_ppc_ibm(n))
+        if (plat == "ppc" || plat == "ppc64" || plat == "ppc64le")
         {
-          if (id == "cpu")
-            description = value;
-          if (id == "revision")
-            version = value;
+          // All cores have same product name and version on power systems
+          if (is_system_ppc_ibm(n))
+            {
+              if (id == "cpu")
+                description = value;
+              if (id == "revision")
+                version = value;
 
-          if (description != "" && version != "")
-          {
-            cpuinfo_ppc_ibm(n, description, version);
-            break;
-          }
+              if (description != "" && version != "")
+              {
+                cpuinfo_ppc_ibm(n, description, version);
+                break;
+              }
+            }
+          else
+            cpuinfo_ppc(n, id, value);
+        }
+        else if (plat == "hppa")
+        {
+          cpuinfo_hppa(n, id, value);
+        }
+        else if (plat == "alpha")
+        {
+          cpuinfo_alpha(n, id, value);
+        }
+        else if (plat == "ia64")
+        {
+          cpuinfo_ia64(n, id, value);
+        }
+        else if (plat == "s390" || plat == "s390x")
+        {
+          cpuinfo_s390x(n, id, value);
+        }
+        else if (plat.compare(0, 3, "arm") == 0)
+        {
+          cpuinfo_arm(n, id, value);
+        }
+        else if (plat == "aarch64")
+        {
+          cpuinfo_aarch64(n, id, value);
         }
         else
-          cpuinfo_ppc(n, id, value);
-#endif
-#ifdef __s390x__
-        cpuinfo_s390x(n, id, value);
-#endif
-#ifdef __hppa__
-        cpuinfo_hppa(n, id, value);
-#endif
-#ifdef __alpha__
-        cpuinfo_alpha(n, id, value);
-#endif
-#ifdef __ia64__
-        cpuinfo_ia64(n, id, value);
-#endif
-
-#ifdef __arm__
-        cpuinfo_arm(n, id, value);
-#endif
-#ifdef __aarch64__
-        cpuinfo_aarch64(n, id, value);
-#endif
-
+        {
+          cpuinfo_x86(n, id, value);
+        }
       }
     }
   }

src/core/dasd.cc
@@ -1,83 +1,0 @@
-#include "disk.h"
-#include "osutils.h"
-#include "dasd.h"
-#include <glob.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <inttypes.h>
-#include <sys/ioctl.h>
-#include <linux/fs.h>
-#include <map>
-
-using namespace std;
-
-/*Read only block devices, not partitions*/
-#define DEV_PATTERN "/dev/dasd[a-z]"
-#define SYSFS_PREFIX "/sys/block/"
-
-bool scan_dasd(hwNode & n)
-{
-  size_t dev_num;
-  char *dev_name;
-  glob_t devices;
-  uint64_t capacity;
-
-  /* These correspond to filenames in the device/ sub-directory
-     of each device
-     To read other attributes, simply add the attribute here and modify the device object
-     appropriately.
-  */
-  const char* attribs[] = {"devtype", "vendor"};
-
-  std::vector<std::string> sysfs_attribs(attribs, attribs+2);
-  std::map<std::string, std::string> dasd_attribs;
-
-  hwNode *core = n.getChild("core");
-
-  /* Probe the sysfs for this device*/
-  if(glob(DEV_PATTERN, 0, NULL, &devices)!=0)
-    return false;
-  else
-  {
-    for(dev_num=0;dev_num<devices.gl_pathc;dev_num++)
-    {
-      dev_name = basename(devices.gl_pathv[dev_num]);
-      for (std::vector<std::string>::iterator it = sysfs_attribs.begin(); it != sysfs_attribs.end(); ++it)
-      {
-        std::string attrib_fname = std::string(SYSFS_PREFIX) + dev_name + "/device/" + *it;
-        std::vector<std::string> lines;
-        if (loadfile(attrib_fname, lines) && (lines.size() > 0))
-        {
-          dasd_attribs[*it] = lines[0];
-        }
-        else
-        {
-          dasd_attribs[*it] = " ";
-        }
-      }
-
-      hwNode device = hwNode("disk", hw::disk);
-      device.setDescription("DASD Device");
-      device.setVendor(dasd_attribs["vendor"]);
-      device.setProduct(dasd_attribs["devtype"]);
-      device.setLogicalName(devices.gl_pathv[dev_num]);
-
-      /* Get the capacity*/
-      int fd = open(devices.gl_pathv[dev_num], O_RDONLY|O_NONBLOCK);
-      if (ioctl(fd, BLKGETSIZE64, &capacity) != 0)
-        capacity=0;
-      close(fd);
-
-      device.setSize(capacity);
-
-      /* Non-determinant*/
-      device.setPhysId(0);
-      scan_disk(device);
-
-      hwNode *parent = core->addChild(device);
-      parent->claim();
-    }
-  }
-  return true;
-}

src/core/dasd.h
@@ -1,7 +1,0 @@
-#ifndef _DASD_H_
-#define _DASD_H_
-
-#include "hw.h"
-
-bool scan_dasd(hwNode & n);
-#endif

src/core/dmi.cc
@@ -1223,7 +1223,7 @@ int dmiversionrev)
                 newnode.setConfig("threads", data[0x25]);
             }
             if (data[0x26] & 0x4)
-              newnode.addCapability("x86-64", "64bits extensions (x86-64)");
+              newnode.addCapability("lm", _("64-bit capable"));
           }
 
           newnode.setHandle(handle);
@@ -1438,39 +1438,52 @@ int dmiversionrev)
       case 16:
 // Physical Memory Array
         {
-          hwNode newnode("memory",
-            hw::memory);
-          string id = "";
+          string id = "memory";
           string description = "";
+          bool claim = false, memory_icon = false;
           switch (data[0x05])
           {
             case 0x03:
               description = _("System Memory");
-              newnode.claim();
-              newnode.addHint("icon", string("memory"));
+              claim = true;
+              memory_icon = true;
               break;
             case 0x04:
+              id = "videomemory";
               description = _("Video Memory");
               break;
             case 0x05:
+              id = "flash";
               description = _("Flash Memory");
               break;
             case 0x06:
+              id = "nvram";
               description = _("NVRAM");
               break;
             case 0x07:
+              id = "cache";
               description = _("Cache Memory");
-              newnode.addHint("icon", string("memory"));
+              memory_icon = true;
               break;
             default:
               description = _("Generic Memory");
-              newnode.addHint("icon", string("memory"));
+              memory_icon = true;
           }
-
+          if (id == "memory" && hardwarenode->getChild("memory"))
+          {
+            // we don't want multiple "System memory" nodes,
+            // so just ignore this one
+            break;
+          }
+          hwNode newnode(id, hw::memory);
           newnode.setHandle(handle);
           newnode.setPhysId(dm->handle);
           newnode.setDescription(description);
           newnode.setSlot(dmi_memory_array_location(data[0x04]));
+          if (memory_icon)
+            newnode.addHint("icon", string("memory"));
+          if (claim)
+            newnode.claim();
           switch (data[0x06])
           {
             case 0x04:
@@ -1598,16 +1611,17 @@ int dmiversionrev)
             newnode.setDescription(newnode.getDescription() + " " + _("[empty]"));
           newnode.setClock(clock);
           hwNode *memoryarray = hardwarenode->findChildByHandle(arrayhandle);
-          if (memoryarray)
-            memoryarray->addChild(newnode);
-          else
+          if (!memoryarray)
+            memoryarray = hardwarenode->getChild("memory");
+          if (!memoryarray)
           {
             hwNode ramnode("memory",
               hw::memory);
-            ramnode.addChild(newnode);
             ramnode.addHint("icon", string("memory"));
             hardwarenode->addChild(ramnode);
+            memoryarray = hardwarenode->getChild("memory");
           }
+          memoryarray->addChild(newnode);
         }
         break;
       case 18:

src/core/hw.cc
@@ -28,7 +28,7 @@ struct hwNode_i
 {
   hwClass deviceclass;
   string id, vendor, product, version, date, serial, slot, handle, description,
-    businfo, physid, dev, modalias;
+    businfo, physid, dev, modalias, subvendor, subproduct;
   bool enabled;
   bool claimed;
   unsigned long long start;
@@ -396,6 +396,22 @@ void hwNode::setVendor(const string & vendor)
 }
 
 
+string hwNode::getSubVendor() const
+{
+  if (This)
+    return This->subvendor;
+  else
+    return "";
+}
+
+
+void hwNode::setSubVendor(const string & subvendor)
+{
+  if (This)
+    This->subvendor = strip(subvendor);
+}
+
+
 string hwNode::getProduct() const
 {
   if (This)
@@ -409,6 +425,22 @@ void hwNode::setProduct(const string & product)
 {
   if (This)
     This->product = strip(product);
+}
+
+
+string hwNode::getSubProduct() const
+{
+  if (This)
+    return This->subproduct;
+  else
+    return "";
+}
+
+
+void hwNode::setSubProduct(const string & subproduct)
+{
+  if (This)
+    This->subproduct = strip(subproduct);
 }
 
 
@@ -1730,6 +1762,24 @@ string hwNode::asXML(unsigned level)
       out << endl;
     }
 
+    if (getSubProduct() != "")
+    {
+      out << spaces(2*level+1);
+      out << "<subproduct>";
+      out << escape(getSubProduct());
+      out << "</subproduct>";
+      out << endl;
+    }
+
+    if (getSubVendor() != "")
+    {
+      out << spaces(2*level+1);
+      out << "<subvendor>";
+      out << escape(getSubVendor());
+      out << "</subvendor>";
+      out << endl;
+    }
+
     if (getBusInfo() != "")
     {
       out << spaces(2*level+1);
@@ -1941,6 +1991,19 @@ string hwNode::asXML(unsigned level)
       out << "</resources>" << endl;
     }
     resources.clear();
+
+    vector < string > hints = getHints();
+    if (hints.size() > 0) {
+      out << spaces(2*level+1);
+      out << "<hints>" << endl;
+      for(unsigned int i=0; i<hints.size(); i++) {
+        out << spaces(2*level+2);
+        out << "<hint name=\"" << hints[i] << "\" " << "value=\"" << getHint(hints[i]).asString() << "\" />";
+        out << endl;
+      }
+      out << spaces(2*level+1);
+      out << "</hints>" << endl;
+    }
   }
 
   for (unsigned int i = 0; i < countChildren(); i++)

commit 1e61e0443086e17821fd598811d2b2cbdb2186c9

devtree: Revert "vendor" property for Power System
Commit f95aa917 removed vendor property for Power System PowerNV platform.
We have "/vendor" property in device tree which gives Vendor name.

Hence revert changes. Note that this only reverts "vendor" property for
particular platform and no side effect on other platform.

Fixes: f95aa917 (merge patches from beaker-fork)
CC: Lyonel Vincent <lyonel@ezix.org>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

src/core/device-tree.cc
@@ -1374,6 +1374,8 @@ bool scan_device_tree(hwNode & n)
   get_ibm_model(n);
   if (matches(get_string(DEVICETREE "/compatible"), "^ibm,powernv"))
   {
+    n.setVendor(get_string(DEVICETREE "/vendor", "IBM"));
+
     if (exists(DEVICETREE "/model-name"))
       n.setProduct(n.getProduct() + " (" +
 		   hw::strip(get_string(DEVICETREE "/model-name")) + ")");