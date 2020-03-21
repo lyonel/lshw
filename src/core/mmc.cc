846a7a19a480f023adfd338e2ae84056f5e83243
Lyonel Vincent <lyonel@ezix.org>
2020-03-21 12:00:45 +0100

    improve detection of SDIO devices

--- src/core/mmc.cc
+++ src/core/mmc.cc
@@ -11,6 +11,80 @@
 
 __ID("@(#) $Id$");
 
+/*
+ * Standard SDIO Function Interfaces
+ */
+
+#define SDIO_CLASS_NONE		0x00	/* Not a SDIO standard interface */
+#define SDIO_CLASS_UART		0x01	/* standard UART interface */
+#define SDIO_CLASS_BT_A		0x02	/* Type-A BlueTooth std interface */
+#define SDIO_CLASS_BT_B		0x03	/* Type-B BlueTooth std interface */
+#define SDIO_CLASS_GPS		0x04	/* GPS standard interface */
+#define SDIO_CLASS_CAMERA	0x05	/* Camera standard interface */
+#define SDIO_CLASS_PHS		0x06	/* PHS standard interface */
+#define SDIO_CLASS_WLAN		0x07	/* WLAN interface */
+#define SDIO_CLASS_ATA		0x08	/* Embedded SDIO-ATA std interface */
+#define SDIO_CLASS_BT_AMP	0x09	/* Type-A Bluetooth AMP interface */
+
+/*
+ * Vendors and devices.  Sort key: vendor first, device next.
+ */
+#define SDIO_VENDOR_ID_BROADCOM			0x02d0
+#define SDIO_DEVICE_ID_BROADCOM_43143		0xa887
+#define SDIO_DEVICE_ID_BROADCOM_43241		0x4324
+#define SDIO_DEVICE_ID_BROADCOM_4329		0x4329
+#define SDIO_DEVICE_ID_BROADCOM_4330		0x4330
+#define SDIO_DEVICE_ID_BROADCOM_4334		0x4334
+#define SDIO_DEVICE_ID_BROADCOM_43340		0xa94c
+#define SDIO_DEVICE_ID_BROADCOM_43341		0xa94d
+#define SDIO_DEVICE_ID_BROADCOM_4335_4339	0x4335
+#define SDIO_DEVICE_ID_BROADCOM_4339		0x4339
+#define SDIO_DEVICE_ID_BROADCOM_43362		0xa962
+#define SDIO_DEVICE_ID_BROADCOM_43364		0xa9a4
+#define SDIO_DEVICE_ID_BROADCOM_43430		0xa9a6
+#define SDIO_DEVICE_ID_BROADCOM_4345		0x4345
+#define SDIO_DEVICE_ID_BROADCOM_43455		0xa9bf
+#define SDIO_DEVICE_ID_BROADCOM_4354		0x4354
+#define SDIO_DEVICE_ID_BROADCOM_4356		0x4356
+#define SDIO_DEVICE_ID_BROADCOM_4359		0x4359
+#define SDIO_DEVICE_ID_CYPRESS_4373		0x4373
+#define SDIO_DEVICE_ID_CYPRESS_43012		43012
+#define SDIO_DEVICE_ID_CYPRESS_89359		0x4355
+
+#define SDIO_VENDOR_ID_INTEL			0x0089
+#define SDIO_DEVICE_ID_INTEL_IWMC3200WIMAX	0x1402
+#define SDIO_DEVICE_ID_INTEL_IWMC3200WIFI	0x1403
+#define SDIO_DEVICE_ID_INTEL_IWMC3200TOP	0x1404
+#define SDIO_DEVICE_ID_INTEL_IWMC3200GPS	0x1405
+#define SDIO_DEVICE_ID_INTEL_IWMC3200BT		0x1406
+#define SDIO_DEVICE_ID_INTEL_IWMC3200WIMAX_2G5	0x1407
+
+#define SDIO_VENDOR_ID_MARVELL			0x02df
+#define SDIO_DEVICE_ID_MARVELL_LIBERTAS		0x9103
+#define SDIO_DEVICE_ID_MARVELL_8688WLAN		0x9104
+#define SDIO_DEVICE_ID_MARVELL_8688BT		0x9105
+#define SDIO_DEVICE_ID_MARVELL_8797_F0		0x9128
+#define SDIO_DEVICE_ID_MARVELL_8887WLAN	0x9134
+
+#define SDIO_VENDOR_ID_MEDIATEK			0x037a
+
+#define SDIO_VENDOR_ID_SIANO			0x039a
+#define SDIO_DEVICE_ID_SIANO_NOVA_B0		0x0201
+#define SDIO_DEVICE_ID_SIANO_NICE		0x0202
+#define SDIO_DEVICE_ID_SIANO_VEGA_A0		0x0300
+#define SDIO_DEVICE_ID_SIANO_VENICE		0x0301
+#define SDIO_DEVICE_ID_SIANO_NOVA_A0		0x1100
+#define SDIO_DEVICE_ID_SIANO_STELLAR 		0x5347
+
+#define SDIO_VENDOR_ID_TI			0x0097
+#define SDIO_DEVICE_ID_TI_WL1271		0x4076
+#define SDIO_VENDOR_ID_TI_WL1251		0x104c
+#define SDIO_DEVICE_ID_TI_WL1251		0x9066
+
+#define SDIO_VENDOR_ID_STE			0x0020
+#define SDIO_DEVICE_ID_STE_CW1200		0x2280
+
+
 using namespace std;
 
 static string strip0x(string s)
@@ -33,6 +107,143 @@ static string manufacturer(unsigned long n)
     default:
             return "Unknown ("+tostring(n)+")";
   }
+}
+
+static hwNode sdioNode(unsigned long long c, unsigned long long vendor = 0, unsigned long long device = 0)
+{
+  hwNode result("interface");
+
+  switch(c)
+  {
+    case SDIO_CLASS_UART:
+      result = hwNode("serial", hw::communication);
+      result.setDescription("UART interface");
+      break;
+    case SDIO_CLASS_BT_A:
+    case SDIO_CLASS_BT_B:
+    case SDIO_CLASS_BT_AMP:
+      result = hwNode("bt", hw::communication);
+      result.setDescription("BlueTooth interface");
+      result.addCapability("wireless");
+      result.addCapability("bluetooth");
+      result.setConfig("wireless", "BlueTooth");
+      break;
+    case SDIO_CLASS_GPS:
+      result = hwNode("gps");
+      result.setDescription("GPS interface");
+      break;
+    case SDIO_CLASS_CAMERA:
+      result = hwNode("camera");
+      result.setDescription("Camera interface");
+      break;
+    case SDIO_CLASS_PHS:
+      result = hwNode("phs");
+      result.setDescription("PHS interface");
+      break;
+    case SDIO_CLASS_WLAN:
+      result = hwNode("wifi", hw::network);
+      result.setDescription("Wireless interface");
+      result.addCapability("wireless");
+      result.setConfig("wireless", "IEEE 802.11");
+      break;
+    case SDIO_CLASS_ATA:
+      result = hwNode("ata");
+      result.setDescription("SDIO-ATA interface");
+      break;
+  }
+
+  switch(vendor)
+  {
+    case SDIO_VENDOR_ID_BROADCOM:
+      result.setVendor("Broadcom");
+      switch(device)
+      {
+        case SDIO_DEVICE_ID_BROADCOM_43143:
+          result.setProduct("43143");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43241:
+          result.setProduct("43241");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4329:
+          result.setProduct("4329");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4330:
+          result.setProduct("4330");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4334:
+          result.setProduct("4334");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43340:
+          result.setProduct("43340");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43341:
+          result.setProduct("43341");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4335_4339:
+          result.setProduct("4335/4339");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4339:
+          result.setProduct("4339");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43362:
+          result.setProduct("43362");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43364:
+          result.setProduct("43364");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43430:
+          result.setProduct("43430");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4345:
+          result.setProduct("4345");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_43455:
+          result.setProduct("43455");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4354:
+          result.setProduct("4354");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4356:
+          result.setProduct("4356");
+          break;
+        case SDIO_DEVICE_ID_BROADCOM_4359:
+          result.setProduct("4359");
+          break;
+        case SDIO_DEVICE_ID_CYPRESS_4373:
+          result.setVendor("Cypress");
+          result.setProduct("4373");
+          break;
+        case SDIO_DEVICE_ID_CYPRESS_43012:
+          result.setVendor("Cypress");
+          result.setProduct("43012");
+          break;
+        case SDIO_DEVICE_ID_CYPRESS_89359:
+          result.setVendor("Cypress");
+          result.setProduct("89359");
+          break;
+      }
+      break;
+    case SDIO_VENDOR_ID_INTEL:
+      result.setVendor("Intel");
+      break;
+    case SDIO_VENDOR_ID_MARVELL:
+      result.setVendor("Marvell");
+      break;
+    case SDIO_VENDOR_ID_MEDIATEK:
+      result.setVendor("MediaTek");
+      break;
+    case SDIO_VENDOR_ID_SIANO:
+      result.setVendor("Siano");
+      break;
+    case SDIO_VENDOR_ID_TI:
+      result.setVendor("Texas Instruments");
+      break;
+    case SDIO_VENDOR_ID_STE:
+      result.setVendor("ST Ericsson");
+      break;
+  }
+
+  return result;
 }
 
 bool scan_mmc(hwNode & n)
@@ -63,10 +274,11 @@ bool scan_mmc(hwNode & n)
     {
       const sysfs::entry & d = *i;
 
-      hwNode card("card");
+      hwNode card("device");
       card.claim();
       string prod = d.string_attr("name");
       if(prod != "00000") card.setProduct(prod);
+      card.addCapability(d.string_attr("type"));
       card.setVendor(manufacturer(d.hex_attr("manfid")));
       card.setBusInfo(guessBusInfo(d.name()));
       card.setPhysId(strip0x(d.string_attr("rca")));
@@ -76,6 +288,8 @@ bool scan_mmc(hwNode & n)
       }
       card.setDate(d.string_attr("date"));
       card.setDescription("SD/MMC Device");
+      if(card.isCapable("sdio"))
+        card.setDescription("SDIO Device");
       if(d.string_attr("scr")!="")
       {
 	card.setDescription("SD Card");
@@ -87,13 +301,17 @@ bool scan_mmc(hwNode & n)
       {
         hwNode *subdev;
         if(devices.size()>1)
-          subdev = card.addChild(hwNode("device"));
+        {
+          subdev = card.addChild(sdioNode(devices[i].hex_attr("class"), devices[i].hex_attr("vendor"), devices[i].hex_attr("device")));
+          subdev->setPhysId(tostring(i+1));
+        }
         else
           subdev = &card;
 
         subdev->setLogicalName(devices[i].name());
         subdev->setModalias(devices[i].modalias());
         subdev->setBusInfo(guessBusInfo(devices[i].name()));
+        subdev->setPhysId(tostring(i+1));
         if(devices[i].hex_attr("removable"))
           subdev->addCapability("removable");
         scan_disk(*subdev);
