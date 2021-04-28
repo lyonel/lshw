lyonel/lshw

commit 8ff1efbc080c804f65f2557f9de3a5057982173d

check SMBIOS version before using Type 17 Extended Size
If the Type 17 size field is 0x7FFF and the SMBIOS is 2.7 or newer, then
the actual memory size is in the Extended Size field.  Otherwise, treat
0x7FFF is the raw and real memory size.

src/core/dmi.cc
@@ -1567,10 +1567,13 @@ int dmiversionrev)
 
 // size
           u = data[0x0D] << 8 | data[0x0C];
-          if(u == 0x7FFF) {
-             unsigned long long extendsize = (data[0x1F] << 24) | (data[0x1E] << 16) | (data[0x1D] << 8) | data[0x1C];
-             extendsize &= 0x7FFFFFFFUL;
-             size = extendsize * 1024ULL * 1024ULL;
+          if ((dmiversionmaj > 2)
+            || ((dmiversionmaj == 2) && (dmiversionmin >= 7))) {
+             if(u == 0x7FFF) {
+                unsigned long long extendsize = (data[0x1F] << 24) | (data[0x1E] << 16) | (data[0x1D] << 8) | data[0x1C];
+                extendsize &= 0x7FFFFFFFUL;
+                size = extendsize * 1024ULL * 1024ULL;
+             }
           }
 	  else
           if (u != 0xFFFF)

commit d01a098f8276bde86630b461be517be53e75e5b4

Fix getting size of memory banks <32GiB
Due to a regression introduced by
8ff1efbc080c804f65f2557f9de3a5057982173d, no size was recorded for
memory banks <32GiB in size on systems with an SMBIOS version of 2.7 or
later. On these systems the Type 17 size field from SMBIOS was only
recorded if the extended size field was used.

Modify the code to use the regular size field whenever it is valid (not
0xFFFF) and not set to 0x7FFF on versions >2.7 (indicating the extended
size field is in use).

src/core/dmi.cc
@@ -1567,15 +1567,14 @@ int dmiversionrev)
 
 // size
           u = data[0x0D] << 8 | data[0x0C];
-          if ((dmiversionmaj > 2)
-            || ((dmiversionmaj == 2) && (dmiversionmin >= 7))) {
-             if(u == 0x7FFF) {
-                unsigned long long extendsize = (data[0x1F] << 24) | (data[0x1E] << 16) | (data[0x1D] << 8) | data[0x1C];
-                extendsize &= 0x7FFFFFFFUL;
-                size = extendsize * 1024ULL * 1024ULL;
-             }
+          if (((dmiversionmaj > 2)
+            || ((dmiversionmaj == 2) && (dmiversionmin >= 7)))
+            && u == 0x7FFF) {
+              unsigned long long extendsize = (data[0x1F] << 24) | (data[0x1E] << 16) | (data[0x1D] << 8) | data[0x1C];
+              extendsize &= 0x7FFFFFFFUL;
+              size = extendsize * 1024ULL * 1024ULL;
           }
-	  else
+          else
           if (u != 0xFFFF)
             size = (1024ULL * (u & 0x7FFF) * ((u & 0x8000) ? 1 : 1024ULL));
           description += string(dmi_memory_device_form_factor(data[0x0E]));